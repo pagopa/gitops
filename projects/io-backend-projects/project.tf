18bdd3cbb9a4dab8ee49b80987dabf93a4cd795a
pasqualedevita <>
2021-02-03 23:15:03 +0100

    moved

--- projects/io-backend-projects/project.tf
+++
@@ -1,7 +0,0 @@
-resource "azuredevops_project" "project" {
-  name               = "io-backend-projects"
-  description        = "This is the DevOps project for all IO backends projects"
-  visibility         = "public"
-  version_control    = "Git"
-  work_item_template = "Basic"
-}
