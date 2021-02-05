d187bbfbe89c90b0d73597cabf9d793b83ec7c84
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-02-05 11:08:13 +0100

    [#176786084] Create io-backend-projects (#3)
    * [#176786084] Create io-backend-projects
    
    * Add Codeowners (#4)
    
    * upgrade to terraform 0.14.5
    
    * remove lock
    
    * [#176786084] Create io-backend-projects
    
    * upgrade to terraform 0.14.5
    
    * remove lock
    
    * move inside azure_devops folder
    
    * moved
    
    * rename folder
    
    * updae devops provider
    
    * Update .gitignore
    
    Co-authored-by: pasqualedevita <>
    Co-authored-by: Emanuele De Cupis <emanuele.decupis@pagopa.it>

---
+++ azure-devops/projects/io-backend-projects/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 0.14.5"
+  backend "azurerm" {
+    resource_group_name  = "io-infra-rg"
+    storage_account_name = "ioinfrastterraform"
+    container_name       = "azuredevopsstate"
+    key                  = "io-backend-projects.terraform.tfstate"
+  }
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 0.1.2"
+    }
+    azurerm = {
+      version  = "~> 2.42.0"
+    }
+    time = {
+      version = "~> 0.6.0"
+    }
+  }
+}
