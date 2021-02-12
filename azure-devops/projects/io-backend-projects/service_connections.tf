03ba1c7fbb36f77d1843cc8fa560b5ccc57a2083
pasqualedevita <>
2021-02-12 10:41:41 +0100

    [#176653851] Add Github Service Connections

--- azure-devops/projects/io-backend-projects/service_connections.tf
+++ azure-devops/projects/io-backend-projects/service_connections.tf
@@ -21,3 +21,31 @@ resource "azuredevops_serviceendpoint_azurerm" "DEV-IO" {
   azurerm_spn_tenantid      = data.azurerm_key_vault_secret.key_vault_secret["TTDIO-SPN-TENANTID"].value
   azurerm_subscription_id   = data.azurerm_key_vault_secret.key_vault_secret["TTDIO-DEV-IO-SUBSCRIPTION-ID"].value
 }
+
+# Github service connection (read-only)
+resource "azuredevops_serviceendpoint_github" "pagopa-github-bot-ro" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "pagopa-github-bot-ro"
+  auth_personal {
+    personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["pagopa-github-bot-ro-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description]
+  }
+}
+
+# Github service connection (read-write)
+resource "azuredevops_serviceendpoint_github" "pagopa-github-bot-rw" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "pagopa-github-bot-rw"
+  auth_personal {
+    personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["pagopa-github-bot-rw-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description]
+  }
+}
