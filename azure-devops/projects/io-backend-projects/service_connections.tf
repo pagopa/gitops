cf9b3f514dcdf1e06b690af870263fd49bb4c47f
pasqualedevita <>
2021-02-12 17:42:19 +0100

    rename service conections

--- azure-devops/projects/io-backend-projects/service_connections.tf
+++ azure-devops/projects/io-backend-projects/service_connections.tf
@@ -23,11 +23,11 @@ resource "azuredevops_serviceendpoint_azurerm" "DEV-IO" {
 }
 
 # Github service connection (read-only)
-resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-ro-TOKEN" {
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-ro" {
   depends_on = [azuredevops_project.project]
 
   project_id            = azuredevops_project.project.id
-  service_endpoint_name = "io-azure-devops-github-ro-TOKEN"
+  service_endpoint_name = "io-azure-devops-github-ro"
   auth_personal {
     personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-ro-TOKEN"].value
   }
@@ -37,11 +37,11 @@ resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-ro-TOKEN" 
 }
 
 # Github service connection (read-write)
-resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-rw-TOKEN" {
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-rw" {
   depends_on = [azuredevops_project.project]
 
   project_id            = azuredevops_project.project.id
-  service_endpoint_name = "io-azure-devops-github-rw-TOKEN"
+  service_endpoint_name = "io-azure-devops-github-rw"
   auth_personal {
     personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-rw-TOKEN"].value
   }
