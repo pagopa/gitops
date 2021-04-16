635b5f88f8e204ed4115bf135fa48271e3aabe44
Simone Infante <simone.infante@pagopa.it>
2021-04-16 20:06:30 +0200

    [IOPAY-136] Delete unnecessary service connection

--- azure-devops/projects/io-pay-projects/service_connections.tf
+++ azure-devops/projects/io-pay-projects/service_connections.tf
@@ -64,18 +64,3 @@ resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-pr" {
   }
 }
 
-# Github service connection (read-only) used to access to azure-pipelines template repo
-resource "azuredevops_serviceendpoint_github" "pagopa" {
-  depends_on = [azuredevops_project.project]
-
-  project_id            = azuredevops_project.project.id
-  service_endpoint_name = "pagopa"
-  auth_personal {
-    personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-ro-TOKEN"].value
-  }
-  lifecycle {
-    ignore_changes = [description, authorization]
-  }
-}
-
-# npm service connection
