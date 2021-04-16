3c4febde9f440205166407167aab9f27731e67a2
Simone Infante <simone.infante@pagopa.it>
2021-04-16 17:19:13 +0200

    [IOPAY-136] new project for io-pay

---
+++ azure-devops/projects/io-pay-projects/project.tf
@@ -0,0 +1,7 @@
+resource "azuredevops_project" "project" {
+  name               = "io-pay-projects"
+  description        = "This is the DevOps project for IO PAY projects"
+  visibility         = "public"
+  version_control    = "Git"
+  work_item_template = "Basic"
+}
---
+++ azure-devops/projects/io-pay-projects/service_connections.tf
@@ -0,0 +1,89 @@
+# Azure service connection PROD-IO
+resource "azuredevops_serviceendpoint_azurerm" "PROD-IO" {
+  depends_on = [azuredevops_project.project]
+
+  project_id                = azuredevops_project.project.id
+  service_endpoint_name     = "PROD-IO-SERVICE-CONN"
+  description               = "PROD-IO Service connection"
+  azurerm_subscription_name = "PROD-IO"
+  azurerm_spn_tenantid      = data.azurerm_key_vault_secret.key_vault_secret["TTDIO-SPN-TENANTID"].value
+  azurerm_subscription_id   = data.azurerm_key_vault_secret.key_vault_secret["TTDIO-PROD-IO-SUBSCRIPTION-ID"].value
+}
+
+# Azure service connection DEV-IO
+resource "azuredevops_serviceendpoint_azurerm" "DEV-IO" {
+  depends_on = [azuredevops_project.project]
+
+  project_id                = azuredevops_project.project.id
+  service_endpoint_name     = "DEV-IO-SERVICE-CONN"
+  description               = "DEV-IO Service connection"
+  azurerm_subscription_name = "DEV-IO"
+  azurerm_spn_tenantid      = data.azurerm_key_vault_secret.key_vault_secret["TTDIO-SPN-TENANTID"].value
+  azurerm_subscription_id   = data.azurerm_key_vault_secret.key_vault_secret["TTDIO-DEV-IO-SUBSCRIPTION-ID"].value
+}
+
+# Github service connection (read-only)
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-ro" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-ro"
+  auth_personal {
+    personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-ro-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description, authorization]
+  }
+}
+
+# Github service connection (read-write)
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-rw" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-rw"
+  auth_personal {
+    personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-rw-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description, authorization]
+  }
+}
+
+# Github service connection (pull request)
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-pr" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-pr"
+  auth_personal {
+    personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-pr-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description, authorization]
+  }
+}
+
+# Github service connection (read-only) used to access to azure-pipelines template repo
+resource "azuredevops_serviceendpoint_github" "pagopa" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "pagopa"
+  auth_personal {
+    personal_access_token = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-ro-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description, authorization]
+  }
+}
+
+# npm service connection
+resource "azuredevops_serviceendpoint_npm" "pagopa-npm-bot" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "pagopa-npm-bot"
+  url                   = "https://registry.npmjs.org"
+  access_token          = data.azurerm_key_vault_secret.key_vault_secret["pagopa-npm-bot-TOKEN"].value
+}
