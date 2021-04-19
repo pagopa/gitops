e5758e1a70867e9b6c512a920ce68474df832b49
Emanuele De Cupis <ema.decu@gmail.com>
2021-04-19 13:32:59 +0200

    configure pipelines for io-functions-assets

---
+++ azure-devops/projects/io-backend-projects/io-functions-assets.tf
@@ -0,0 +1,159 @@
+variable "io-functions-assets" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "io-functions-assets"
+      branch_name    = "master"
+      pipelines_path = ".devops"
+    }
+    pipeline = {
+      cache_version_id = "v3"
+    }
+  }
+}
+
+#
+# Code Review pipeline
+#
+
+# Define code review pipeline
+resource "azuredevops_build_definition" "io-functions-assets-code-review" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-pr, azuredevops_project.project]
+
+  project_id = azuredevops_project.project.id
+  name       = "${var.io-functions-assets.repository.name}.code-review"
+  path       = "\\${var.io-functions-assets.repository.name}"
+
+  pull_request_trigger {
+    initial_branch = var.io-functions-assets.repository.branch_name
+    forks {
+      enabled       = false
+      share_secrets = false
+    }
+    override {
+      auto_cancel = false
+      branch_filter {
+        include = [var.io-functions-assets.repository.branch_name]
+      }
+      path_filter {
+        exclude = []
+        include = []
+      }
+    }
+  }
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.io-functions-assets.repository.organization}/${var.io-functions-assets.repository.name}"
+    branch_name           = var.io-functions-assets.repository.branch_name
+    yml_path              = "${var.io-functions-assets.repository.pipelines_path}/code-review-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  }
+
+  variable {
+    name         = "DANGER_GITHUB_API_TOKEN"
+    secret_value = data.azurerm_key_vault_secret.key_vault_secret["DANGER-GITHUB-API-TOKEN"].value
+    is_secret    = true
+  }
+}
+
+# Allow code review pipeline to access Github readonly service connection, needed to access external templates to be used inside the pipeline
+resource "azuredevops_resource_authorization" "io-functions-assets-code-review-github-ro-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.pagopa, azuredevops_build_definition.io-functions-assets-code-review, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.pagopa.id
+  definition_id = azuredevops_build_definition.io-functions-assets-code-review.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow code review pipeline to access Github pr service connection, needed to checkout code from the pull request branch
+resource "azuredevops_resource_authorization" "io-functions-assets-code-review-github-pr-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-pr, azuredevops_build_definition.io-functions-assets-code-review, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  definition_id = azuredevops_build_definition.io-functions-assets-code-review.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+#
+# Deploy pipeline
+#
+
+# Define deploy pipeline
+resource "azuredevops_build_definition" "io-functions-assets-deploy" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_serviceendpoint_azurerm.PROD-IO, azuredevops_project.project]
+
+  project_id = azuredevops_project.project.id
+  name       = "${var.io-functions-assets.repository.name}.deploy"
+  path       = "\\${var.io-functions-assets.repository.name}"
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.io-functions-assets.repository.organization}/${var.io-functions-assets.repository.name}"
+    branch_name           = var.io-functions-assets.repository.branch_name
+    yml_path              = "${var.io-functions-assets.repository.pipelines_path}/deploy-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  }
+
+  variable {
+    name  = "GIT_EMAIL"
+    value = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-EMAIL"].value
+  }
+
+  variable {
+    name  = "GIT_USERNAME"
+    value = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-USERNAME"].value
+  }
+
+  variable {
+    name  = "GITHUB_CONNECTION"
+    value = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.service_endpoint_name
+  }
+
+  variable {
+    name  = "CACHE_VERSION_ID"
+    value = var.io-functions-assets.pipeline.cache_version_id
+  }
+
+  variable {
+    name  = "PRODUCTION_AZURE_SUBSCRIPTION"
+    value = azuredevops_serviceendpoint_azurerm.PROD-IO.service_endpoint_name
+  }
+}
+
+# Allow deploy pipeline to access Github readonly service connection, needed to access external templates to be used inside the pipeline
+resource "azuredevops_resource_authorization" "io-functions-assets-deploy-github-ro-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.pagopa, azuredevops_build_definition.io-functions-assets-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.pagopa.id
+  definition_id = azuredevops_build_definition.io-functions-assets-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow deploy pipeline to access Github writable service connection, needed to bump project version and publish a new relase
+resource "azuredevops_resource_authorization" "io-functions-assets-deploy-github-rw-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_build_definition.io-functions-assets-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  definition_id = azuredevops_build_definition.io-functions-assets-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow deploy pipeline to access Azure PROD-IO subscription service connection, needed to interact with Azure resources
+resource "azuredevops_resource_authorization" "io-functions-assets-deploy-azurerm-PROD-IO-auth" {
+  depends_on = [azuredevops_serviceendpoint_azurerm.PROD-IO, azuredevops_build_definition.io-functions-assets-deploy, time_sleep.wait]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_azurerm.PROD-IO.id
+  definition_id = azuredevops_build_definition.io-functions-assets-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
