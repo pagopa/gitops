7952210f83ca92e50b04b892776fea5dc97d3e6c
Walter Traspadini <walter.traspadini@sorint.it>
2021-04-20 18:07:05 +0200

    [#IOINFRA-70] refactor hubpa project + module secrets

---
+++ azure-devops/projects/hub-pa-projects.old/secrets.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "secrets" {
+  default = [
+    "DANGER-GITHUB-API-TOKEN",
+    "io-azure-devops-github-ro-TOKEN",
+    "io-azure-devops-github-rw-TOKEN",
+    "io-azure-devops-github-pr-TOKEN",
+    "io-azure-devops-github-EMAIL",
+    "io-azure-devops-github-USERNAME",
+    "sonarqube-TOKEN",
+    "sonarqube-URL",
+    "TTDIO-PROD-HUBPA-SUBSCRIPTION-ID",
+    "TTDIO-DEV-HUBPA-SUBSCRIPTION-ID",
+    "TTDIO-SPN-TENANTID",
+  ]
+}
+
+data "azurerm_key_vault" "keyvault" {
+  name                = "io-p-kv-azuredevops"
+  resource_group_name = "io-p-rg-operations"
+}
+
+data "azurerm_key_vault_secret" "key_vault_secret" {
+  for_each     = toset(var.secrets)
+  name         = each.value
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
