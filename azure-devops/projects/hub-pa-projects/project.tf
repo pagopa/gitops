0bc023ced27c5bc0d4ca37c7bc70b8d2b26a47af
Walter Traspadini <walter.traspadini@sorint.it>
2021-04-20 18:06:17 +0200

    [#IOINFRA-70] refactor hubpa project + module secrets

--- azure-devops/projects/hub-pa-projects/project.tf
+++ azure-devops/projects/hub-pa-projects/project.tf
@@ -1,6 +1,6 @@
 resource "azuredevops_project" "project" {
   name               = "hub-pa-projects"
-  description        = "This is the DevOps project for all hub pa projects"
+  description        = "This is the DevOps project for Hub Pa projects"
   visibility         = "public"
   version_control    = "Git"
   work_item_template = "Basic"
