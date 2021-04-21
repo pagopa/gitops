5b3d8c37f6148b46cdc366b896f597118298b117
Simone infante <52280205+infantesimone@users.noreply.github.com>
2021-04-21 14:20:51 +0200

    Update azure-devops/projects/io-pay-projects/io-pay-portal-fe.tf
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

--- azure-devops/projects/io-pay-projects/io-pay-portal-fe.tf
+++ azure-devops/projects/io-pay-projects/io-pay-portal-fe.tf
@@ -212,3 +212,11 @@ resource "azuredevops_resource_authorization" "io-pay-portal-fe-deploy-azurerm-P
   authorized    = true
   type          = "endpoint"
 }
+
+resource "azurerm_role_assignment" "io-pay-deploy-azurerm-PROD-IO-iopstcdniopayportal" {
+  depends_on = [data.azuread_service_principal.service_principals]
+
+  principal_id         = data.azuread_service_principal.service_principals[local.PROD-IO-UID].id
+  role_definition_name = "Storage Blob Data Contributor"
+  scope                = "/subscriptions/${data.azurerm_key_vault_secret.key_vault_secret["TTDIO-PROD-IO-SUBSCRIPTION-ID"].value}/resourceGroups/${var.io-pay-portal-fe.pipeline.resource_group_azure}/providers/Microsoft.Storage/storageAccounts/${var.io-pay-portal-fe.pipeline.production_storage_account_name}"
+}
