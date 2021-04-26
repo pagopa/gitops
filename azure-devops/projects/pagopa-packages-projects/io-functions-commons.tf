3c0745d64a3d52f89086adde9e7f2f0f82545f7c
Emanuele De Cupis <emanuele.decupis@pagopa.it>
2021-04-26 12:42:05 +0200

    [#IP-157] Configure pipelines for `io-functions-commons` (#36)
    * add npm pacakges project
    
    * add io-functions-commons pipeline
    
    * rename project
    
    * update provider
    
    * remove unused service connection
    
    * refactor project
    
    * dedicated state storage key
    
    * add allow_override false, remove unused dependency
    
    * fix allow_override NPM_CONNECTION
    
    * rename project
    
    Co-authored-by: pasqualedevita <>

---
+++ azure-devops/projects/pagopa-packages-projects/io-functions-commons.tf
@@ -0,0 +1,166 @@
+variable "io-functions-commons" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "io-functions-commons"
+      branch_name    = "master"
+      pipelines_path = ".devops"
+    }
+    pipeline = {
+      cache_version_id = "v3"
+    }
+  }
+}
+
+#
+# Code Review pipeline
+#
+
+# Define code review pipeline
+resource "azuredevops_build_definition" "io-functions-commons-code-review" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-pr, azuredevops_project.project]
+
+  project_id = azuredevops_project.project.id
+  name       = "${var.io-functions-commons.repository.name}.code-review"
+  path       = "\\${var.io-functions-commons.repository.name}"
+
+  pull_request_trigger {
+    initial_branch = var.io-functions-commons.repository.branch_name
+    forks {
+      enabled       = false
+      share_secrets = false
+    }
+    override {
+      auto_cancel = false
+      branch_filter {
+        include = [var.io-functions-commons.repository.branch_name]
+      }
+      path_filter {
+        exclude = []
+        include = []
+      }
+    }
+  }
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.io-functions-commons.repository.organization}/${var.io-functions-commons.repository.name}"
+    branch_name           = var.io-functions-commons.repository.branch_name
+    yml_path              = "${var.io-functions-commons.repository.pipelines_path}/code-review-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  }
+
+  variable {
+    name           = "DANGER_GITHUB_API_TOKEN"
+    secret_value   = data.azurerm_key_vault_secret.key_vault_secret["DANGER-GITHUB-API-TOKEN"].value
+    is_secret      = true
+    allow_override = false
+  }
+}
+
+# Allow code review pipeline to access Github readonly service connection, needed to access external templates to be used inside the pipeline
+resource "azuredevops_resource_authorization" "io-functions-commons-code-review-github-ro-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.pagopa, azuredevops_build_definition.io-functions-commons-code-review, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.pagopa.id
+  definition_id = azuredevops_build_definition.io-functions-commons-code-review.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow code review pipeline to access Github pr service connection, needed to checkout code from the pull request branch
+resource "azuredevops_resource_authorization" "io-functions-commons-code-review-github-pr-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-pr, azuredevops_build_definition.io-functions-commons-code-review, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  definition_id = azuredevops_build_definition.io-functions-commons-code-review.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+#
+# Deploy pipeline
+#
+
+# Define deploy pipeline
+resource "azuredevops_build_definition" "io-functions-commons-deploy" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_project.project]
+
+  project_id = azuredevops_project.project.id
+  name       = "${var.io-functions-commons.repository.name}.deploy"
+  path       = "\\${var.io-functions-commons.repository.name}"
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.io-functions-commons.repository.organization}/${var.io-functions-commons.repository.name}"
+    branch_name           = var.io-functions-commons.repository.branch_name
+    yml_path              = "${var.io-functions-commons.repository.pipelines_path}/deploy-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  }
+
+  variable {
+    name           = "GIT_EMAIL"
+    value          = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-EMAIL"].value
+    allow_override = false
+  }
+
+  variable {
+    name           = "GIT_USERNAME"
+    value          = data.azurerm_key_vault_secret.key_vault_secret["io-azure-devops-github-USERNAME"].value
+    allow_override = false
+  }
+
+  variable {
+    name           = "GITHUB_CONNECTION"
+    value          = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.service_endpoint_name
+    allow_override = false
+  }
+
+  variable {
+    name           = "NPM_CONNECTION"
+    value          = azuredevops_serviceendpoint_npm.pagopa-npm-bot.service_endpoint_name
+    allow_override = false
+  }
+
+  variable {
+    name           = "CACHE_VERSION_ID"
+    value          = var.io-functions-commons.pipeline.cache_version_id
+    allow_override = false
+  }
+
+}
+
+# Allow deploy pipeline to access Github readonly service connection, needed to access external templates to be used inside the pipeline
+resource "azuredevops_resource_authorization" "io-functions-commons-deploy-github-ro-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.pagopa, azuredevops_build_definition.io-functions-commons-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.pagopa.id
+  definition_id = azuredevops_build_definition.io-functions-commons-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow deploy pipeline to access Github writable service connection, needed to bump project version and publish a new relase
+resource "azuredevops_resource_authorization" "io-functions-commons-deploy-github-rw-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_build_definition.io-functions-commons-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  definition_id = azuredevops_build_definition.io-functions-commons-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow deploy pipeline to access NPM service connection, needed to publish sdk packages to the public registry
+resource "azuredevops_resource_authorization" "io-functions-commons-deploy-npm-auth" {
+  depends_on = [azuredevops_serviceendpoint_npm.pagopa-npm-bot, azuredevops_build_definition.io-functions-commons-deploy, time_sleep.wait]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_npm.pagopa-npm-bot.id
+  definition_id = azuredevops_build_definition.io-functions-commons-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
