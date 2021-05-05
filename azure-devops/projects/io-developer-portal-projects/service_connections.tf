34dc86efbce6072a0d46535638b88ec4e4186bae
pasqualedevita <>
2021-05-05 11:08:11 +0200

    remove unused service connection

--- azure-devops/projects/io-developer-portal-projects/service_connections.tf
+++ azure-devops/projects/io-developer-portal-projects/service_connections.tf
@@ -83,17 +83,3 @@ resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-pr" {
     ignore_changes = [description, authorization]
   }
 }
-
-# Github service connection (read-only) used to access to azure-pipelines template repo
-resource "azuredevops_serviceendpoint_github" "pagopa" {
-  depends_on = [azuredevops_project.project]
-
-  project_id            = azuredevops_project.project.id
-  service_endpoint_name = "pagopa"
-  auth_personal {
-    personal_access_token = module.secrets.values["io-azure-devops-github-ro-TOKEN"].value
-  }
-  lifecycle {
-    ignore_changes = [description, authorization]
-  }
-}
