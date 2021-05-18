53799856d9bf1aea8443af84f4e46cb6ad0c489b
pp-ps <pietro.stroia@pagopa.it>
2021-05-18 18:01:44 +0200

    add certaz project for io-italia-it

--- azure-devops/projects/cert-az-management/service_connections.tf
+++ azure-devops/projects/cert-az-management/service_connections.tf
@@ -74,3 +74,26 @@ resource "azuredevops_serviceendpoint_azurerm" "PROD-GCNPORTAL" {
   azurerm_spn_tenantid      = module.secrets.values["PAGOPAIT-TENANTID"].value
   azurerm_subscription_id   = module.secrets.values["PAGOPAIT-PROD-GCNPORTAL-SUBSCRIPTION-ID"].value
 }
+
+# Production service connection PROD-IO
+resource "azuredevops_serviceendpoint_azurerm" "PROD-IO" {
+  depends_on = [azuredevops_project.project]
+
+  project_id                = azuredevops_project.project.id
+  service_endpoint_name     = "PROD-IO-SERVICE-CONN"
+  description               = "PROD-IO Service connection"
+  azurerm_subscription_name = "PROD-IO"
+  azurerm_spn_tenantid      = module.secrets.values["TTDIO-SPN-TENANTID"].value
+  azurerm_subscription_id   = module.secrets.values["TTDIO-PROD-IO-SUBSCRIPTION-ID"].value
+}
+
+# Azure service connection DEV-IO
+resource "azuredevops_serviceendpoint_azurerm" "DEV-IO" {
+  depends_on = [azuredevops_project.project]
+
+  project_id                = azuredevops_project.project.id
+  service_endpoint_name     = "DEV-IO-SERVICE-CONN"
+  description               = "DEV-IO Service connection"
+  azurerm_subscription_name = "DEV-IO"
+  azurerm_spn_tenantid      = module.secrets.values["TTDIO-SPN-TENANTID"].value
+  azurerm_subscription_id   = module.secrets.values["TTDIO-DEV-IO-SUBSCRIPTION-ID"].value
