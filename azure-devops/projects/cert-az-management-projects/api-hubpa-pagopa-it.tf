cf3b208966842aecf876f006359e71472489d0f9
Giovanni <g.mancini.0@gmail.com>
2021-05-19 13:54:03 +0200

    [PTT-129] Create Pipeline for production certificates (#65)
    * [PTT-129] Create Pipeline for production certificates
    
    * some fix, rename folder
    
    * Update azure-devops/projects/cert-az-management/api.hubpa.pagopa.it.tf
    
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
    
    Co-authored-by: pasqualedevita <>
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

---
+++ azure-devops/projects/cert-az-management-projects/api-hubpa-pagopa-it.tf
@@ -0,0 +1,69 @@
+variable "api-hubpa-pagopa-it" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "cert-az-management"
+      branch_name    = "master"
+      pipelines_path = "."
+    }
+    pipeline = {
+      enable_cert_az = true
+      path           = "hubpa"
+      name           = "api.hubpa.pagopa.it"
+      # common variables to all pipelines
+      variables = {
+        DO_RENEW_CERT               = "true"
+        PRODUCTION_AcmeDirectory    = "LE_PROD"
+        PRODUCTION_CertificateNames = "api.hubpa.pagopa.it"
+        PRODUCTION_ResourceGroup    = "hubpa-p-sec-rg"
+        PRODUCTION_KeyVault         = "hubpa-p-key-vault"
+        TEST_AcmeDirectory          = "LE_STAGE"
+        TEST_CertificateNames       = "NA"
+        TEST_ResourceGroup          = "NA"
+        TEST_KeyVault               = "NA"
+      }
+      # common secret variables to all pipelines
+      variables_secret = {
+      }
+    }
+  }
+}
+
+locals {
+  api-hubpa-pagopa-it-variables = {
+    PRODUCTION_AcmeContact        = module.secrets.values["CERT-AZ-MANAGEMENT-MAIL-CONTACT"].value
+    PRODUCTION_AZURE_SUBSCRIPTION = azuredevops_serviceendpoint_azurerm.DEV-HUBPA.service_endpoint_name
+    PRODUCTION_KeyVaultResourceId = "/subscriptions/${module.secrets.values["PAGOPAIT-PROD-HUBPA-SUBSCRIPTION-ID"].value}/resourceGroups/${var.api-hubpa-pagopa-it.pipeline.variables.PRODUCTION_ResourceGroup}/providers/Microsoft.KeyVault/vaults/${var.api-hubpa-pagopa-it.pipeline.variables.PRODUCTION_KeyVault}"
+    TEST_AcmeContact              = "NA"
+    TEST_AZURE_SUBSCRIPTION       = azuredevops_serviceendpoint_azurerm.DEV-HUBPA.service_endpoint_name
+    TEST_KeyVaultResourceId       = "/subscriptions/${module.secrets.values["PAGOPAIT-PROD-HUBPA-SUBSCRIPTION-ID"].value}/resourceGroups/${var.api-hubpa-pagopa-it.pipeline.variables.TEST_ResourceGroup}/providers/Microsoft.KeyVault/vaults/${var.api-hubpa-pagopa-it.pipeline.variables.TEST_KeyVault}"
+  }
+  api-hubpa-pagopa-it-variables_secret = {
+  }
+}
+
+module "api-hubpa-pagopa-it-cert_az" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_certaz?ref=v0.0.2"
+  count  = var.api-hubpa-pagopa-it.pipeline.enable_cert_az == true ? 1 : 0
+
+  project_id                   = azuredevops_project.project.id
+  repository                   = var.api-hubpa-pagopa-it.repository
+  name                         = var.api-hubpa-pagopa-it.pipeline.name
+  path                         = var.api-hubpa-pagopa-it.pipeline.path
+  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+
+  variables = merge(
+    var.api-hubpa-pagopa-it.pipeline.variables,
+    local.api-hubpa-pagopa-it-variables,
+  )
+
+  variables_secret = merge(
+    var.api-hubpa-pagopa-it.pipeline.variables_secret,
+    local.api-hubpa-pagopa-it-variables_secret,
+  )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
+    azuredevops_serviceendpoint_azurerm.PROD-HUBPA.id,
+  ]
+}
