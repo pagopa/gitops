51d1380a53305aaf7ce6cc67b848d2f7244650c0
pasqualedevita <>
2021-05-20 13:35:34 +0200

    fix az cert az subscription

--- azure-devops/projects/cert-az-management-projects/api-hubpa-pagopa-it.tf
+++ azure-devops/projects/cert-az-management-projects/api-hubpa-pagopa-it.tf
@@ -32,10 +32,10 @@ variable "api-hubpa-pagopa-it" {
 locals {
   api-hubpa-pagopa-it-variables = {
     PRODUCTION_AcmeContact        = module.secrets.values["CERT-AZ-MANAGEMENT-MAIL-CONTACT"].value
-    PRODUCTION_AZURE_SUBSCRIPTION = azuredevops_serviceendpoint_azurerm.DEV-HUBPA.service_endpoint_name
+    PRODUCTION_AZURE_SUBSCRIPTION = azuredevops_serviceendpoint_azurerm.PROD-HUBPA.service_endpoint_name
     PRODUCTION_KeyVaultResourceId = "/subscriptions/${module.secrets.values["PAGOPAIT-PROD-HUBPA-SUBSCRIPTION-ID"].value}/resourceGroups/${var.api-hubpa-pagopa-it.pipeline.variables.PRODUCTION_ResourceGroup}/providers/Microsoft.KeyVault/vaults/${var.api-hubpa-pagopa-it.pipeline.variables.PRODUCTION_KeyVault}"
     TEST_AcmeContact              = "NA"
-    TEST_AZURE_SUBSCRIPTION       = azuredevops_serviceendpoint_azurerm.DEV-HUBPA.service_endpoint_name
+    TEST_AZURE_SUBSCRIPTION       = azuredevops_serviceendpoint_azurerm.PROD-HUBPA.service_endpoint_name
     TEST_KeyVaultResourceId       = "/subscriptions/${module.secrets.values["PAGOPAIT-PROD-HUBPA-SUBSCRIPTION-ID"].value}/resourceGroups/${var.api-hubpa-pagopa-it.pipeline.variables.TEST_ResourceGroup}/providers/Microsoft.KeyVault/vaults/${var.api-hubpa-pagopa-it.pipeline.variables.TEST_KeyVault}"
   }
   api-hubpa-pagopa-it-variables_secret = {
