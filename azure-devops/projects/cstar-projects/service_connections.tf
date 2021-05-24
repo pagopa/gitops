edca7de9f14b54fef2cbfadd8bf114a5c61661f2
pasqualedevita <>
2021-05-24 10:24:18 +0200

    add DEV service connection

--- azure-devops/projects/cstar-projects/service_connections.tf
+++ azure-devops/projects/cstar-projects/service_connections.tf
@@ -31,6 +31,18 @@ locals {
   azuredevops_serviceendpoint_sonarcloud_id = 1
 }
 
+# Dev service connection
+resource "azuredevops_serviceendpoint_azurerm" "DEV-CSTAR" {
+  depends_on = [azuredevops_project.project]
+
+  project_id                = azuredevops_project.project.id
+  service_endpoint_name     = "DEV-CSTAR-SERVICE-CONN"
+  description               = "DEV-CSTAR Service connection"
+  azurerm_subscription_name = "DEV-CSTAR"
+  azurerm_spn_tenantid      = module.secrets.values["PAGOPAIT-TENANTID"].value
+  azurerm_subscription_id   = module.secrets.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
+}
+
 # # azure container registry dev service connection
 # resource "azuredevops_serviceendpoint_azurecr" "cstar-azurecr-dev" {
 #   depends_on = [azuredevops_project.project]
