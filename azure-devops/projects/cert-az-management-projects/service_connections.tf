e3063243b8c96080a1a622273d1916301784907d
uolter <walter.traspadini@pagopa.it>
2021-07-08 12:15:54 +0200

    pipeline to generate cstar dev certificates

--- azure-devops/projects/cert-az-management-projects/service_connections.tf
+++ azure-devops/projects/cert-az-management-projects/service_connections.tf
@@ -98,3 +98,16 @@ resource "azuredevops_serviceendpoint_azurerm" "DEV-IO" {
   azurerm_spn_tenantid      = module.secrets.values["TTDIO-SPN-TENANTID"].value
   azurerm_subscription_id   = module.secrets.values["TTDIO-DEV-IO-SUBSCRIPTION-ID"].value
 }
+
+# CSTAR #
+
+resource "azuredevops_serviceendpoint_azurerm" "DEV-CSTAR" {
+  depends_on = [azuredevops_project.project]
+
+  project_id                = azuredevops_project.project.id
+  service_endpoint_name     = "DEV-CSTAR-SERVICE-CONN"
+  description               = "DEV-CSTAR Service connection"
+  azurerm_subscription_name = "DEV-CSTAR"
+  azurerm_spn_tenantid      = module.secrets.values["PAGOPAIT-TENANTID"].value
+  azurerm_subscription_id   = module.secrets.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
+}
