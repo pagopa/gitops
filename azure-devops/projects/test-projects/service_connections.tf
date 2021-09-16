40347366166f2413c117995ee0e9e64c8483a759
pasqualedevita <>
2021-09-16 17:23:10 +0200

    test env

--- azure-devops/projects/test-projects/service_connections.tf
+++ azure-devops/projects/test-projects/service_connections.tf
@@ -26,14 +26,17 @@ resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-pr" {
   }
 }
 
-# DEV-CSTAR service connection
-resource "azuredevops_serviceendpoint_azurerm" "DEV-CSTAR" {
+module "DEV-CSTAR-TLS-CERT-SERVICE-CONN" {
   depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=add-acme-tiny"
 
-  project_id                = azuredevops_project.project.id
-  service_endpoint_name     = "DEV-CSTAR-SERVICE-CONN"
-  description               = "DEV-CSTAR Service connection"
-  azurerm_subscription_name = "DEV-CSTAR"
-  azurerm_spn_tenantid      = module.secrets.values["PAGOPAIT-TENANTID"].value
-  azurerm_subscription_id   = module.secrets.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
+  project_id        = azuredevops_project.project.id
+  name              = "cstar-d-tls-cert"
+  tenant_id         = module.secrets.values["PAGOPAIT-TENANTID"].value
+  subscription_id   = module.secrets.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
+  subscription_name = "DEV-CSTAR"
+
+  credential_subcription              = local.key_vault_subscription
+  credential_key_vault_name           = local.key_vault_name
+  credential_key_vault_resource_group = local.key_vault_resource_group
 }
--- azure-devops/projects/test-projects/tlscert-testcert-cstar-dev-pagopa-it.tf
+++ azure-devops/projects/test-projects/tlscert-testcert-cstar-dev-pagopa-it.tf
@@ -7,13 +7,15 @@ variable "tlscert-testcert-cstar-dev-pagopa-it" {
       pipelines_path = "."
     }
     pipeline = {
-      enable_tls_cert = true
-      path            = "TLS-Certificates"
-      name            = "testcert.dev.cstar.pagopa.it"
+      enable_tls_cert         = true
+      path                    = "TLS-Certificates"
+      dns_record_name         = "testcert"
+      dns_zone_name           = "dev.cstar.pagopa.it"
+      dns_zone_resource_group = "cstar-d-vnet-rg"
       # common variables to all pipelines
       variables = {
-        CERT_NAME_EXPIRE_SECONDS = "2592000" #30 days
-        KEY_VAULT_NAME           = "cstar-d-kv"
+        CERT_EXPIRE_DAYS = "30"
+        KEY_VAULT_NAME   = "cstar-d-kv"
       }
       # common secret variables to all pipelines
       variables_secret = {
@@ -23,24 +25,39 @@ variable "tlscert-testcert-cstar-dev-pagopa-it" {
 }
 
 locals {
+  tlscert-testcert-cstar-dev-pagopa-it = {
+    tenant_id         = module.secrets.values["PAGOPAIT-TENANTID"].value
+    subscription_name = "DEV-CSTAR"
+    subscription_id   = module.secrets.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
+  }
   tlscert-testcert-cstar-dev-pagopa-it-variables = {
-    KEY_VAULT_CERT_NAME          = replace(var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.name, ".", "-")
-    KEY_VAULT_SERVICE_CONNECTION = azuredevops_serviceendpoint_azurerm.DEV-CSTAR.service_endpoint_name
+    KEY_VAULT_CERT_NAME          = "${replace(var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.dns_record_name, ".", "-")}-${replace(var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.dns_zone_name, ".", "-")}"
+    KEY_VAULT_SERVICE_CONNECTION = module.DEV-CSTAR-TLS-CERT-SERVICE-CONN.service_endpoint_name
   }
   tlscert-testcert-cstar-dev-pagopa-it-variables_secret = {
   }
 }
 
 module "tlscert-testcert-cstar-dev-pagopa-it-cert_az" {
-  # source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_certaz?ref=v0.0.2"
-  source = "/Users/pasqualedevita/Documents/github/azuredevops-tf-modules/azuredevops_build_definition_tls_cert"
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert?ref=add-acme-tiny"
   count  = var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
   repository                   = var.tlscert-testcert-cstar-dev-pagopa-it.repository
-  name                         = var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.name
+  name                         = "${var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.dns_record_name}.${var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.dns_zone_name}"
   path                         = var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.path
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id
+
+  dns_record_name         = var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.dns_record_name
+  dns_zone_name           = var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.dns_zone_name
+  dns_zone_resource_group = var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.dns_zone_resource_group
+  tenant_id               = local.tlscert-testcert-cstar-dev-pagopa-it.tenant_id
+  subscription_name       = local.tlscert-testcert-cstar-dev-pagopa-it.subscription_name
+  subscription_id         = local.tlscert-testcert-cstar-dev-pagopa-it.subscription_id
+
+  credential_subcription              = local.key_vault_subscription
+  credential_key_vault_name           = local.key_vault_name
+  credential_key_vault_resource_group = local.key_vault_resource_group
 
   variables = merge(
     var.tlscert-testcert-cstar-dev-pagopa-it.pipeline.variables,
@@ -53,6 +70,6 @@ module "tlscert-testcert-cstar-dev-pagopa-it-cert_az" {
   )
 
   service_connection_ids_authorization = [
-    azuredevops_serviceendpoint_azurerm.DEV-CSTAR.id,
+    module.DEV-CSTAR-TLS-CERT-SERVICE-CONN.service_endpoint_id,
   ]
 }
