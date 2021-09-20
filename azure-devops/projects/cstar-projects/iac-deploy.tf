ea01d3a2f659cfc14c13cb109fe3b4f3a5ec24b6
uolter <walter.traspadini@pagopa.it>
2021-09-20 12:05:31 +0200

    iac pipeline for cstar

--- azure-devops/projects/cstar-projects/iac-deploy.tf
+++ azure-devops/projects/cstar-projects/iac-deploy.tf
@@ -17,8 +17,6 @@ variable "iac-builds" {
 locals {
   # global vars
   iac-builds-variables = {
-    tenant_id = var.tenant_id
-
   }
   # global secrets
   iac-builds-variables_secret = {
@@ -35,33 +33,6 @@ locals {
   }
 }
 
-/*
-module "iac-builds_code_review" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v1.0.0"
-  count  = var.iac-builds.pipeline.enable_code_review == true ? 1 : 0
-
-  project_id                   = azuredevops_project.project.id
-  repository                   = var.iac-builds.repository
-  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
-
-  pull_request_trigger_use_yaml = true
-
-  variables = merge(
-    local.iac-builds-variables,
-    local.iac-builds-variables_code_review,
-  )
-
-  variables_secret = merge(
-    local.iac-builds-variables_secret,
-    local.iac-builds-variables_secret_code_review,
-  )
-
-  service_connection_ids_authorization = [
-    azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
-    local.azuredevops_serviceendpoint_sonarcloud_id,
-  ]
-}
-*/
 
 module "iac-builds_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v1.0.0"
