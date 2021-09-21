dc19371ac9da4d77ac7152791e1bdf9c5aad34b9
pasqualedevita <>
2021-09-21 11:19:08 +0200

    Changes to cstar-iac project

--- azure-devops/projects/cstar-iac-projects/iac.tf
+++ azure-devops/projects/cstar-iac-projects/iac.tf
@@ -1,9 +1,9 @@
-variable "iac-builds" {
+variable "iac" {
   default = {
     repository = {
       organization    = "pagopa"
       name            = "cstar-infrastructure"
-      branch_name     = "master"
+      branch_name     = "main"
       pipelines_path  = ".devops"
       yml_prefix_name = null
     }
@@ -16,45 +16,54 @@ variable "iac-builds" {
 
 locals {
   # global vars
-  iac-builds-variables = {
+  iac-variables = {
   }
   # global secrets
-  iac-builds-variables_secret = {
+  iac-variables_secret = {
+
+  }
+
+  # code_review vars
+  iac-variables_code_review = {
+
+  }
+  # code_review secrets
+  iac-variables_secret_code_review = {
 
   }
 
   # deploy vars
-  iac-builds-variables_deploy = {
+  iac-variables_deploy = {
 
   }
   # deploy secrets
-  iac-builds-variables_secret_deploy = {
+  iac-variables_secret_deploy = {
 
   }
 }
 
-
-module "iac-builds_deploy" {
+module "iac_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v1.0.0"
-  count  = var.iac-builds.pipeline.enable_deploy == true ? 1 : 0
+  count  = var.iac.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.iac-builds.repository
+  repository                   = var.iac.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
 
   ci_trigger_use_yaml = true
 
   variables = merge(
-    local.iac-builds-variables,
-    local.iac-builds-variables_deploy,
+    local.iac-variables,
+    local.iac-variables_deploy,
   )
 
   variables_secret = merge(
-    local.iac-builds-variables_secret,
-    local.iac-builds-variables_secret_deploy,
+    local.iac-variables_secret,
+    local.iac-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
     azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
+    azuredevops_serviceendpoint_azurerm.DEV-CSTAR.id,
   ]
 }
