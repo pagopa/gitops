7de1890c5df9a7629c998f54b005985e4e3478af
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-09-29 09:01:40 +0200

    split deploy-code review for iac (#135)
    Co-authored-by: pasqualedevita <>

--- azure-devops/projects/cstar-iac-projects/iac.tf
+++ azure-devops/projects/cstar-iac-projects/iac.tf
@@ -8,7 +8,7 @@ variable "iac" {
       yml_prefix_name = null
     }
     pipeline = {
-      enable_code_review = false
+      enable_code_review = true
       enable_deploy      = true
     }
   }
@@ -17,6 +17,7 @@ variable "iac" {
 locals {
   # global vars
   iac-variables = {
+
   }
   # global secrets
   iac-variables_secret = {
@@ -40,6 +41,34 @@ locals {
   iac-variables_secret_deploy = {
 
   }
+}
+
+module "iac_code_review" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v1.0.0"
+  count  = var.iac.pipeline.enable_code_review == true ? 1 : 0
+
+  project_id                   = azuredevops_project.project.id
+  repository                   = var.iac.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+
+  pull_request_trigger_use_yaml = true
+
+  variables = merge(
+    local.iac-variables,
+    local.iac-variables_code_review,
+  )
+
+  variables_secret = merge(
+    local.iac-variables_secret,
+    local.iac-variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
+    azuredevops_serviceendpoint_azurerm.DEV-CSTAR.id,
+    azuredevops_serviceendpoint_azurerm.UAT-CSTAR.id,
+    azuredevops_serviceendpoint_azurerm.PROD-CSTAR.id,
+  ]
 }
 
 module "iac_deploy" {
