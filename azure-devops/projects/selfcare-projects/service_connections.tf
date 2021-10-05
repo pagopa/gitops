2d41f54f33c256e68001ded66c70899967d400f9
anttorre <antonio.torre@everis.com>
2021-10-05 17:27:01 +0200

    service connection for tlscert

--- azure-devops/projects/selfcare-projects/service_connections.tf
+++ azure-devops/projects/selfcare-projects/service_connections.tf
@@ -81,8 +81,52 @@ resource "azuredevops_serviceendpoint_azurerm" "PROD-SELFCARE" {
   azurerm_subscription_id   = module.secrets.values["PAGOPAIT-PROD-SELFCARE-SUBSCRIPTION-ID"].value
 }
 
-/* TODO uncomment after creation of registry and aks
+module "DEV-SELFCARE-TLS-CERT-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v1.1.0"
 
+  project_id        = azuredevops_project.project.id
+  name              = "selc-d-tls-cert"
+  tenant_id         = module.secrets.values["PAGOPAIT-TENANTID"].value
+  subscription_id   = module.secrets.values["PAGOPAIT-DEV-SELFCARE-SUBSCRIPTION-ID"].value
+  subscription_name = "DEV-SelfCare"
+
+  credential_subcription              = local.key_vault_subscription
+  credential_key_vault_name           = local.key_vault_name
+  credential_key_vault_resource_group = local.key_vault_resource_group
+}
+
+module "UAT-SELFCARE-TLS-CERT-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v1.1.0"
+
+  project_id        = azuredevops_project.project.id
+  name              = "selc-u-tls-cert"
+  tenant_id         = module.secrets.values["PAGOPAIT-TENANTID"].value
+  subscription_id   = module.secrets.values["PAGOPAIT-UAT-SELFCARE-SUBSCRIPTION-ID"].value
+  subscription_name = "UAT-SelfCare"
+
+  credential_subcription              = local.key_vault_subscription
+  credential_key_vault_name           = local.key_vault_name
+  credential_key_vault_resource_group = local.key_vault_resource_group
+}
+
+module "PROD-SELFCARE-TLS-CERT-SERVICE-CONN" {
+  depends_on = [azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v1.1.0"
+
+  project_id        = azuredevops_project.project.id
+  name              = "selc-p-tls-cert"
+  tenant_id         = module.secrets.values["PAGOPAIT-TENANTID"].value
+  subscription_id   = module.secrets.values["PAGOPAIT-PROD-SELFCARE-SUBSCRIPTION-ID"].value
+  subscription_name = "PROD-SelfCare"
+
+  credential_subcription              = local.key_vault_subscription
+  credential_key_vault_name           = local.key_vault_name
+  credential_key_vault_resource_group = local.key_vault_resource_group
+}
+
+/* TODO uncomment after creation of registry and aks
 # DEV service connection for azure container registry
 resource "azuredevops_serviceendpoint_azurecr" "selfcare-azurecr-dev" {
   depends_on = [azuredevops_project.project]
