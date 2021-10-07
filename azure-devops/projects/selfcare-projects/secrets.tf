91bc22cfba1a86f75ef3eb80093411f9b7e663f7
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2021-10-07 11:19:07 +0200

    Selfcare devops (#145)
    * selfcare projects base devops
    
    * aks and registry configuration commented
    
    * service connection for tlscert
    
    * Apply suggestions from code review
    
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
    
    * Update azure-devops/projects/selfcare-projects/service_connections.tf
    
    Co-authored-by: anttorre <antonio.torre@everis.com>
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

---
+++ azure-devops/projects/selfcare-projects/secrets.tf
@@ -0,0 +1,19 @@
+module "secrets" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
+
+  resource_group = "io-p-rg-operations"
+  key_vault_name = "io-p-kv-azuredevops"
+
+  secrets = [
+    "DANGER-GITHUB-API-TOKEN",
+    "io-azure-devops-github-ro-TOKEN",
+    "io-azure-devops-github-rw-TOKEN",
+    "io-azure-devops-github-pr-TOKEN",
+    "io-azure-devops-github-EMAIL",
+    "io-azure-devops-github-USERNAME",
+    "PAGOPAIT-TENANTID",
+    "PAGOPAIT-DEV-SELFCARE-SUBSCRIPTION-ID",
+    "PAGOPAIT-UAT-SELFCARE-SUBSCRIPTION-ID",
+    "PAGOPAIT-PROD-SELFCARE-SUBSCRIPTION-ID",
+  ]
+}
