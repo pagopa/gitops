6f4e0acec3282147b78cc6e46410ce93d30214c0
anttorre <antonio.torre@everis.com>
2021-10-14 17:21:42 +0200

    deploy pipeline for hub-spid-login

--- azure-devops/projects/selfcare-projects/hub-spid-login-ms.tf
+++ azure-devops/projects/selfcare-projects/hub-spid-login-ms.tf
@@ -4,11 +4,10 @@ variable "hub-spid-login-ms" {
       organization    = "antonioT90" #TODO fixme after PR to merge k8s devops
       name            = "hub-spid-login-ms"
       branch_name     = "master"
-      pipelines_path  = ".devops"
+      pipelines_path  = ".devops/kubernetes"
       yml_prefix_name = null
     }
     pipeline = {
-      enable_code_review = true
       enable_deploy      = true
     }
   }
@@ -40,7 +39,7 @@ locals {
     dev_container_registry_name          = "selcdacr.azurecr.io"
     dev_agent_pool                       = "selc-dev-linux"
     dev_replicas                         = 1
-    uat_container_registry_service_conn  = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-uat.service_endpoint_name
+/*    uat_container_registry_service_conn  = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-uat.service_endpoint_name
     uat_kubernetes_service_conn          = azuredevops_serviceendpoint_kubernetes.selfcare-aks-uat.service_endpoint_name
     uat_container_registry_name          = "selcuacr.azurecr.io"
     uat_agent_pool                       = "selc-uat-linux"
@@ -49,37 +48,12 @@ locals {
     prod_kubernetes_service_conn         = azuredevops_serviceendpoint_kubernetes.selfcare-aks-prod.service_endpoint_name
     prod_container_registry_name         = "selcpacr.azurecr.io"
     prod_agent_pool                      = "selc-prod-linux"
-    prod_replicas                        = 2
+    prod_replicas                        = 2*/
   }
   # deploy secrets
   hub-spid-login-ms-variables_secret_deploy = {
 
   }
-}
-
-module "hub-spid-login-ms_code_review" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v1.0.0"
-  count  = var.hub-spid-login-ms.pipeline.enable_code_review == true ? 1 : 0
-
-  project_id                   = azuredevops_project.project.id
-  repository                   = var.hub-spid-login-ms.repository
-  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
-
-  pull_request_trigger_use_yaml = true
-
-  variables = merge(
-  local.hub-spid-login-ms-variables,
-  local.hub-spid-login-ms-variables_code_review,
-  )
-
-  variables_secret = merge(
-  local.hub-spid-login-ms-variables_secret,
-  local.hub-spid-login-ms-variables_secret_code_review,
-  )
-
-  service_connection_ids_authorization = [
-    azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id
-  ]
 }
 
 module "hub-spid-login-ms_deploy" {
@@ -105,13 +79,13 @@ module "hub-spid-login-ms_deploy" {
   service_connection_ids_authorization = [
     azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
     azuredevops_serviceendpoint_azurerm.DEV-SELFCARE.id,
-    azuredevops_serviceendpoint_azurerm.UAT-SELFCARE.id,
-    azuredevops_serviceendpoint_azurerm.PROD-SELFCARE.id,
+/*    azuredevops_serviceendpoint_azurerm.UAT-SELFCARE.id,
+    azuredevops_serviceendpoint_azurerm.PROD-SELFCARE.id,*/
     azuredevops_serviceendpoint_azurecr.selfcare-azurecr-dev.id,
     azuredevops_serviceendpoint_kubernetes.selfcare-aks-dev.id,
-    azuredevops_serviceendpoint_azurecr.selfcare-azurecr-uat.id,
+/*    azuredevops_serviceendpoint_azurecr.selfcare-azurecr-uat.id,
     azuredevops_serviceendpoint_kubernetes.selfcare-aks-uat.id,
     azuredevops_serviceendpoint_azurecr.selfcare-azurecr-prod.id,
-    azuredevops_serviceendpoint_kubernetes.selfcare-aks-prod.id,
+    azuredevops_serviceendpoint_kubernetes.selfcare-aks-prod.id,*/
   ]
 }
--- azure-devops/projects/selfcare-projects/service_connections.tf
+++ azure-devops/projects/selfcare-projects/service_connections.tf
@@ -178,7 +178,7 @@ resource "azuredevops_serviceendpoint_kubernetes" "selfcare-aks-dev" {
     ca_cert = module.secrets.values["dev-selfcare-aks-azure-devops-sa-cacrt"].value
   }
 }
-
+/*
 # UAT service connection for azure kubernetes service
 resource "azuredevops_serviceendpoint_kubernetes" "selfcare-aks-uat" {
   depends_on            = [azuredevops_project.project]
@@ -205,4 +205,4 @@ resource "azuredevops_serviceendpoint_kubernetes" "selfcare-aks-prod" {
     token   = module.secrets.values["prod-selfcare-aks-azure-devops-sa-token"].value
     ca_cert = module.secrets.values["prod-selfcare-aks-azure-devops-sa-cacrt"].value
   }
-}
+}*/
