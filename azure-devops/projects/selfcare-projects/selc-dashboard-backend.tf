f77f16dcba547c5faad34e22f378f0e1e6f6e9de
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2021-10-21 11:10:49 +0200

    Selfcare dashboard backend (#171)
    * hub-spid-login deploy
    
    * deploy pipeline for hub-spid-login
    
    * fixed namespace reference and added required secrets
    
    * pipeline for dashboard-backend application
    
    * fixed selfhosted agent names
    
    Co-authored-by: anttorre <antonio.torre@everis.com>

---
+++ azure-devops/projects/selfcare-projects/selc-dashboard-backend.tf
@@ -0,0 +1,122 @@
+variable "selc-dashboard-backend" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "selfcare-dashboard-backend"
+      branch_name     = "master"
+      pipelines_path  = ".devops"
+      yml_prefix_name = null
+    }
+    pipeline = {
+      enable_code_review = true
+      enable_deploy      = true
+    }
+  }
+}
+
+locals {
+  # global vars
+  selc-dashboard-backend-variables = {
+    dockerfile = "Dockerfile"
+  }
+  # global secrets
+  selc-dashboard-backend-variables_secret = {
+
+  }
+  # code_review vars
+  selc-dashboard-backend-variables_code_review = {
+    sonarcloud_service_conn = "SONARCLOUD-SERVICE-CONN"
+    sonarcloud_org          = var.selc-dashboard-backend.repository.organization
+    sonarcloud_project_key  = "${var.selc-dashboard-backend.repository.organization}_${var.selc-dashboard-backend.repository.name}"
+    sonarcloud_project_name = var.selc-dashboard-backend.repository.name
+  }
+  # code_review secrets
+  selc-dashboard-backend-variables_secret_code_review = {
+
+  }
+  # deploy vars
+  selc-dashboard-backend-variables_deploy = {
+    k8s_image_repository_name            = replace(var.selc-dashboard-backend.repository.name, "-", "")
+    deploy_namespace                     = "selc"
+    settings_xml_rw_secure_file_name     = "settings-rw.xml"
+    settings_xml_ro_secure_file_name     = "settings-ro.xml"
+    dev_container_registry_service_conn  = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-dev.service_endpoint_name
+    dev_kubernetes_service_conn          = azuredevops_serviceendpoint_kubernetes.selfcare-aks-dev.service_endpoint_name
+    dev_container_registry_name          = "selcdacr.azurecr.io"
+    dev_agent_pool                       = "selfcare-dev-linux"
+//    uat_container_registry_service_conn  = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-uat.service_endpoint_name TODO uncomment when aks UAT will be available
+//    uat_kubernetes_service_conn          = azuredevops_serviceendpoint_kubernetes.selfcare-aks-uat.service_endpoint_name TODO uncomment when aks UAT will be available
+    uat_container_registry_name          = "selcuacr.azurecr.io"
+    uat_agent_pool                       = "selfcare-uat-linux"
+//    prod_container_registry_service_conn = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-prod.service_endpoint_name TODO uncomment when aks UAT will be available
+//    prod_kubernetes_service_conn         = azuredevops_serviceendpoint_kubernetes.selfcare-aks-prod.service_endpoint_name TODO uncomment when aks UAT will be available
+    prod_container_registry_name         = "selcpacr.azurecr.io"
+    prod_agent_pool                      = "selfcare-prod-linux"
+  }
+  # deploy secrets
+  selc-dashboard-backend-variables_secret_deploy = {
+
+  }
+}
+
+module "selc-dashboard-backend_code_review" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v1.0.0"
+  count  = var.selc-dashboard-backend.pipeline.enable_code_review == true ? 1 : 0
+
+  project_id                   = azuredevops_project.project.id
+  repository                   = var.selc-dashboard-backend.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+
+  pull_request_trigger_use_yaml = true
+
+  variables = merge(
+    local.selc-dashboard-backend-variables,
+    local.selc-dashboard-backend-variables_code_review,
+  )
+
+  variables_secret = merge(
+    local.selc-dashboard-backend-variables_secret,
+    local.selc-dashboard-backend-variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
+    local.azuredevops_serviceendpoint_sonarcloud_id,
+  ]
+}
+
+module "selc-dashboard-backend_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v1.0.0"
+  count  = var.selc-dashboard-backend.pipeline.enable_deploy == true ? 1 : 0
+
+  project_id                   = azuredevops_project.project.id
+  repository                   = var.selc-dashboard-backend.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+
+  ci_trigger_use_yaml = true
+
+  variables = merge(
+    local.selc-dashboard-backend-variables,
+    local.selc-dashboard-backend-variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.selc-dashboard-backend-variables_secret,
+    local.selc-dashboard-backend-variables_secret_deploy,
+  )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
+    azuredevops_serviceendpoint_azurerm.DEV-SELFCARE.id,
+//    azuredevops_serviceendpoint_azurerm.UAT-SELFCARE.id, TODO uncomment when aks UAT will be available
+//    azuredevops_serviceendpoint_azurerm.PROD-SELFCARE.id, TODO uncomment when aks PROD will be available
+    azuredevops_serviceendpoint_azurecr.selfcare-azurecr-dev.id,
+    azuredevops_serviceendpoint_kubernetes.selfcare-aks-dev.id,
+/* TODO uncomment when aks UAT will be available
+    azuredevops_serviceendpoint_azurecr.selfcare-azurecr-uat.id,
+    azuredevops_serviceendpoint_kubernetes.selfcare-aks-uat.id,*/
+/* TODO uncomment when aks PROD will be available
+    azuredevops_serviceendpoint_azurecr.selfcare-azurecr-prod.id,
+    azuredevops_serviceendpoint_kubernetes.selfcare-aks-prod.id,*/
+  ]
+}
