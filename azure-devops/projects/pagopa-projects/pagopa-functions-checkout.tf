458f42113d9a4706e6c0b8c2d43e395e537deb02
Simone Infante <simone.infante@pagopa.it>
2021-10-22 10:03:40 +0200

    update repo

--- azure-devops/projects/pagopa-projects/pagopa-functions-checkout.tf
+++ azure-devops/projects/pagopa-projects/pagopa-functions-checkout.tf
@@ -1,10 +1,10 @@
-variable "pagopa-checkout-function" {
+variable "pagopa-functions-checkout" {
   default = {
     repository = {
       organization    = "pagopa"
-      name            = "io-pay-portal"
+      name            = "pagopa-functions-checkout"
       branch_name     = "main"
-      pipelines_path  = "./io-functions-pay-portal/.devops"
+      pipelines_path  = ".devops"
       yml_prefix_name = "pagopa"
     }
     pipeline = {
@@ -16,24 +16,24 @@ variable "pagopa-checkout-function" {
 
 locals {
   # global vars
-  pagopa-checkout-function-variables = {
+  pagopa-functions-checkout-variables = {
     cache_version_id = "v1"
-    default_branch   = var.pagopa-checkout-function.repository.branch_name
+    default_branch   = var.pagopa-functions-checkout.repository.branch_name
   }
   # global secrets
-  pagopa-checkout-function-variables_secret = {
+  pagopa-functions-checkout-variables_secret = {
 
   }
   # code_review vars
-  pagopa-checkout-function-variables_code_review = {
+  pagopa-functions-checkout-variables_code_review = {
     danger_github_api_token = "skip"
   }
   # code_review secrets
-  pagopa-checkout-function-variables_secret_code_review = {
+  pagopa-functions-checkout-variables_secret_code_review = {
 
   }
   # deploy vars
-  pagopa-checkout-function-variables_deploy = {
+  pagopa-functions-checkout-variables_deploy = {
     git_mail                        = module.secrets.values["io-azure-devops-github-EMAIL"].value
     git_username                    = module.secrets.values["io-azure-devops-github-USERNAME"].value
     github_connection               = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.service_endpoint_name
@@ -41,27 +41,27 @@ locals {
     prod_azure_subscription         = azuredevops_serviceendpoint_azurerm.PROD-PAGOPA.service_endpoint_name
   }
   # deploy secrets
-  pagopa-checkout-function-variables_secret_deploy = {
+  pagopa-functions-checkout-variables_secret_deploy = {
 
   }
 }
 
-module "pagopa-checkout-function_code_review" {
+module "pagopa-functions-checkout_code_review" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v1.0.0"
-  count  = var.pagopa-checkout-function.pipeline.enable_code_review == true ? 1 : 0
+  count  = var.pagopa-functions-checkout.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.pagopa-checkout-function.repository
+  repository                   = var.pagopa-functions-checkout.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
 
   variables = merge(
-    local.pagopa-checkout-function-variables,
-    local.pagopa-checkout-function-variables_code_review,
+    local.pagopa-functions-checkout-variables,
+    local.pagopa-functions-checkout-variables_code_review,
   )
 
   variables_secret = merge(
-    local.pagopa-checkout-function-variables_secret,
-    local.pagopa-checkout-function-variables_secret_code_review,
+    local.pagopa-functions-checkout-variables_secret,
+    local.pagopa-functions-checkout-variables_secret_code_review,
   )
 
   service_connection_ids_authorization = [
@@ -69,22 +69,22 @@ module "pagopa-checkout-function_code_review" {
   ]
 }
 
-module "pagopa-checkout-function_deploy" {
+module "pagopa-functions-checkout_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v1.0.0"
-  count  = var.pagopa-checkout-function.pipeline.enable_deploy == true ? 1 : 0
+  count  = var.pagopa-functions-checkout.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.pagopa-checkout-function.repository
+  repository                   = var.pagopa-functions-checkout.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
 
   variables = merge(
-    local.pagopa-checkout-function-variables,
-    local.pagopa-checkout-function-variables_deploy,
+    local.pagopa-functions-checkout-variables,
+    local.pagopa-functions-checkout-variables_deploy,
   )
 
   variables_secret = merge(
-    local.pagopa-checkout-function-variables_secret,
-    local.pagopa-checkout-function-variables_secret_deploy,
+    local.pagopa-functions-checkout-variables_secret,
+    local.pagopa-functions-checkout-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
