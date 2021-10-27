ba7a49662cab3748efbe92268ebd811f7fa8a6fe
pasqualedevita <>
2021-10-27 11:58:29 +0200

    add frontend packages

---
+++ azure-devops/projects/pagopa-packages-frontend-projects/service_connections.tf
@@ -0,0 +1,51 @@
+# Github service connection (read-only)
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-ro" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-ro"
+  auth_personal {
+    personal_access_token = module.secrets.values["io-azure-devops-github-ro-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description, authorization]
+  }
+}
+
+# Github service connection (pull request)
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-pr" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-pr"
+  auth_personal {
+    personal_access_token = module.secrets.values["io-azure-devops-github-pr-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description, authorization]
+  }
+}
+
+# Github service connection (read-write)
+resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-rw" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "io-azure-devops-github-rw"
+  auth_personal {
+    personal_access_token = module.secrets.values["io-azure-devops-github-rw-TOKEN"].value
+  }
+  lifecycle {
+    ignore_changes = [description, authorization]
+  }
+}
+
+# npm service connection test
+resource "azuredevops_serviceendpoint_npm" "pagopa-npm-bot" {
+  depends_on = [azuredevops_project.project]
+
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "pagopa-npm-bot"
+  url                   = "https://registry.npmjs.org"
+  access_token          = module.secrets.values["pagopa-npm-bot-TOKEN-test"].value
+}
