e22e07f8c18e412387f733e25204feb8f06f5355
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2021-10-27 12:03:59 +0200

    dashboard and onboarding FE pipelines (#178)
    refactor to define common FE deployment variables
    
    Co-authored-by: anttorre <antonio.torre@everis.com>

--- azure-devops/projects/selfcare-projects/selc-fe-onboarding.tf
+++ azure-devops/projects/selfcare-projects/selc-fe-onboarding.tf
@@ -1,8 +1,8 @@
-variable "selc-fe-login" {
+variable "selc-fe-onboarding" {
   default = {
     repository = {
       organization    = "pagopa"
-      name            = "selfcare-login-frontend"
+      name            = "selfcare-onboarding-frontend"
       branch_name     = "main"
       pipelines_path  = ".devops"
       yml_prefix_name = null
@@ -16,47 +16,47 @@ variable "selc-fe-login" {
 
 locals {
   # global vars
-  selc-fe-login-variables = {
-    default_branch   = var.selc-fe-login.repository.branch_name
+  selc-fe-onboarding-variables = {
+    default_branch   = var.selc-fe-onboarding.repository.branch_name
   }
   # global secrets
-  selc-fe-login-variables_secret = {
+  selc-fe-onboarding-variables_secret = {
 
   }
   # code_review vars
-  selc-fe-login-variables_code_review = {
+  selc-fe-onboarding-variables_code_review = {
     danger_github_api_token = "skip"
   }
   # code_review secrets
-  selc-fe-login-variables_secret_code_review = {
+  selc-fe-onboarding-variables_secret_code_review = {
 
   }
   # deploy vars
-  selc-fe-login-variables_deploy = {
+  selc-fe-onboarding-variables_deploy = {
 
   }
   # deploy secrets
-  selc-fe-login-variables_secret_deploy = {
+  selc-fe-onboarding-variables_secret_deploy = {
 
   }
 }
 
-module "selc-fe-login_code_review" {
+module "selc-fe-onboarding_code_review" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v1.0.0"
-  count  = var.selc-fe-login.pipeline.enable_code_review == true ? 1 : 0
+  count  = var.selc-fe-onboarding.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.selc-fe-login.repository
+  repository                   = var.selc-fe-onboarding.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
 
   variables = merge(
-    local.selc-fe-login-variables,
-    local.selc-fe-login-variables_code_review,
+    local.selc-fe-onboarding-variables,
+    local.selc-fe-onboarding-variables_code_review,
   )
 
   variables_secret = merge(
-    local.selc-fe-login-variables_secret,
-    local.selc-fe-login-variables_secret_code_review,
+    local.selc-fe-onboarding-variables_secret,
+    local.selc-fe-onboarding-variables_secret_code_review,
   )
 
   service_connection_ids_authorization = [
@@ -64,25 +64,25 @@ module "selc-fe-login_code_review" {
   ]
 }
 
-module "selc-fe-login_deploy" {
+module "selc-fe-onboarding_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v1.0.0"
-  count  = var.selc-fe-login.pipeline.enable_deploy == true ? 1 : 0
+  count  = var.selc-fe-onboarding.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.selc-fe-login.repository
+  repository                   = var.selc-fe-onboarding.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
 
   ci_trigger_use_yaml = true
 
   variables = merge(
     local.selc-fe-common-variables_deploy,
-    local.selc-fe-login-variables,
-    local.selc-fe-login-variables_deploy,
+    local.selc-fe-onboarding-variables,
+    local.selc-fe-onboarding-variables_deploy,
   )
 
   variables_secret = merge(
-    local.selc-fe-login-variables_secret,
-    local.selc-fe-login-variables_secret_deploy,
+    local.selc-fe-onboarding-variables_secret,
+    local.selc-fe-onboarding-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
