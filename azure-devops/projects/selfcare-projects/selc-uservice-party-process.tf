ef6f616fdfdddedb344a649baaa043cd1bb86eaf
pasqualedevita <>
2021-10-27 17:56:04 +0200

    add selfcare-uservice-party-process

--- azure-devops/projects/selfcare-projects/selc-uservice-party-process.tf
+++ azure-devops/projects/selfcare-projects/selc-uservice-party-process.tf
@@ -1,4 +1,4 @@
-variable "selfc-uservice-party-process" {
+variable "selc-uservice-party-process" {
   default = {
     repository = {
       organization    = "pagopa"
@@ -16,50 +16,66 @@ variable "selfc-uservice-party-process" {
 
 locals {
   # global vars
-  selfc-uservice-party-process-variables = {
-
+  selc-uservice-party-process-variables = {
+    docker_base_image_name = "ghcr.io/pagopa/pdnd-interop-uservice-party-process"
+    dockerfile             = "Dockerfile"
   }
   # global secrets
-  selfc-uservice-party-process-variables_secret = {
+  selc-uservice-party-process-variables_secret = {
 
   }
   # code_review vars
-  selfc-uservice-party-process-variables_code_review = {
+  selc-uservice-party-process-variables_code_review = {
 
   }
   # code_review secrets
-  selfc-uservice-party-process-variables_secret_code_review = {
+  selc-uservice-party-process-variables_secret_code_review = {
 
   }
   # deploy vars
-  selfc-uservice-party-process-variables_deploy = {
-
+  selc-uservice-party-process-variables_deploy = {
+    k8s_image_repository_name              = replace(replace(var.selc-uservice-party-process.repository.name, "-", ""), "selfcare", "")
+    deploy_namespace                       = "selc"
+    common_container_registry_name         = "ghcr.io"
+    common_container_registry_service_conn = azuredevops_serviceendpoint_dockerregistry.github_docker_registry_ro.service_endpoint_name
+    dev_container_registry_service_conn    = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-dev.service_endpoint_name
+    dev_kubernetes_service_conn            = azuredevops_serviceendpoint_kubernetes.selfcare-aks-dev.service_endpoint_name
+    dev_container_registry_name            = "selcdacr.azurecr.io"
+    dev_agent_pool                         = "selfcare-dev-linux"
+    //    uat_container_registry_service_conn  = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-uat.service_endpoint_name TODO uncomment when aks UAT will be available
+    //    uat_kubernetes_service_conn          = azuredevops_serviceendpoint_kubernetes.selfcare-aks-uat.service_endpoint_name TODO uncomment when aks UAT will be available
+    uat_container_registry_name = "selcuacr.azurecr.io"
+    uat_agent_pool              = "selfcare-uat-linux"
+    //    prod_container_registry_service_conn = azuredevops_serviceendpoint_azurecr.selfcare-azurecr-prod.service_endpoint_name TODO uncomment when aks UAT will be available
+    //    prod_kubernetes_service_conn         = azuredevops_serviceendpoint_kubernetes.selfcare-aks-prod.service_endpoint_name TODO uncomment when aks UAT will be available
+    prod_container_registry_name = "selcpacr.azurecr.io"
+    prod_agent_pool              = "selfcare-prod-linux"
   }
   # deploy secrets
-  selfc-uservice-party-process-variables_secret_deploy = {
-    DOCKER_REGISTRY_PAGOPA_USER     = module.secrets.values["SELC-DOCKER-REGISTRY-PAGOPA-USER"].value
-    DOCKER_REGISTRY_PAGOPA_TOKEN_RO = module.secrets.values["SELC-DOCKER-REGISTRY-PAGOPA-TOKEN-RO"].value
+  selc-uservice-party-process-variables_secret_deploy = {
+    # docker_registry_pagopa_user     = module.secrets.values["SELC-DOCKER-REGISTRY-PAGOPA-USER"].value
+    # docker_registry_pagopa_token_ro = module.secrets.values["SELC-DOCKER-REGISTRY-PAGOPA-TOKEN-RO"].value
   }
 }
 
-module "selfc-uservice-party-process_code_review" {
+module "selc-uservice-party-process_code_review" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v1.0.0"
-  count  = var.selfc-uservice-party-process.pipeline.enable_code_review == true ? 1 : 0
+  count  = var.selc-uservice-party-process.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.selfc-uservice-party-process.repository
+  repository                   = var.selc-uservice-party-process.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
 
   pull_request_trigger_use_yaml = true
 
   variables = merge(
-    local.selfc-uservice-party-process-variables,
-    local.selfc-uservice-party-process-variables_code_review,
+    local.selc-uservice-party-process-variables,
+    local.selc-uservice-party-process-variables_code_review,
   )
 
   variables_secret = merge(
-    local.selfc-uservice-party-process-variables_secret,
-    local.selfc-uservice-party-process-variables_secret_code_review,
+    local.selc-uservice-party-process-variables_secret,
+    local.selc-uservice-party-process-variables_secret_code_review,
   )
 
   service_connection_ids_authorization = [
@@ -68,28 +84,29 @@ module "selfc-uservice-party-process_code_review" {
   ]
 }
 
-module "selfc-uservice-party-process_deploy" {
+module "selc-uservice-party-process_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v1.0.0"
-  count  = var.selfc-uservice-party-process.pipeline.enable_deploy == true ? 1 : 0
+  count  = var.selc-uservice-party-process.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
-  repository                   = var.selfc-uservice-party-process.repository
+  repository                   = var.selc-uservice-party-process.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
 
   ci_trigger_use_yaml = true
 
   variables = merge(
-    local.selfc-uservice-party-process-variables,
-    local.selfc-uservice-party-process-variables_deploy,
+    local.selc-uservice-party-process-variables,
+    local.selc-uservice-party-process-variables_deploy,
   )
 
   variables_secret = merge(
-    local.selfc-uservice-party-process-variables_secret,
-    local.selfc-uservice-party-process-variables_secret_deploy,
+    local.selc-uservice-party-process-variables_secret,
+    local.selc-uservice-party-process-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
     azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
+    azuredevops_serviceendpoint_dockerregistry.github_docker_registry_ro.id,
     azuredevops_serviceendpoint_azurecr.selfcare-azurecr-dev.id,
     azuredevops_serviceendpoint_kubernetes.selfcare-aks-dev.id,
   ]
--- azure-devops/projects/selfcare-projects/service_connections.tf
+++ azure-devops/projects/selfcare-projects/service_connections.tf
@@ -40,6 +40,16 @@ resource "azuredevops_serviceendpoint_github" "io-azure-devops-github-rw" {
   }
 }
 
+# Github docker registry (read-only)
+resource "azuredevops_serviceendpoint_dockerregistry" "github_docker_registry_ro" {
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "github-docker-registry-ro"
+  docker_registry       = "https://ghcr.io"
+  docker_username       = module.secrets.values["SELC-DOCKER-REGISTRY-PAGOPA-USER"].value
+  docker_password       = module.secrets.values["SELC-DOCKER-REGISTRY-PAGOPA-TOKEN-RO"].value
+  registry_type         = "Others"
+}
+
 # TODO azure devops terraform provider does not support SonarCloud service endpoint
 locals {
   azuredevops_serviceendpoint_sonarcloud_id = "4126bdf1-3b2f-4e34-b9e7-4508d8de8ebe"
