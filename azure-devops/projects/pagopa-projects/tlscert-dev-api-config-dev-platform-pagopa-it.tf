06a17f0908649d1735891efa23c539b998894d3d
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-11-10 15:56:29 +0100

    fix apiconfig dns_record_name (#190)
    Co-authored-by: pasqualedevita <>

--- azure-devops/projects/pagopa-projects/tlscert-dev-api-config-dev-platform-pagopa-it.tf
+++ azure-devops/projects/pagopa-projects/tlscert-dev-api-config-dev-platform-pagopa-it.tf
@@ -9,8 +9,8 @@ variable "tlscert-dev-api-config-dev-platform-pagopa-it" {
     pipeline = {
       enable_tls_cert         = true
       path                    = "TLS-Certificates\\DEV"
-      dns_record_name         = ""
-      dns_zone_name           = "apiconfig.dev.platform.pagopa.it"
+      dns_record_name         = "apiconfig"
+      dns_zone_name           = "dev.platform.pagopa.it"
       dns_zone_resource_group = "pagopa-d-vnet-rg"
       # common variables to all pipelines
       variables = {
