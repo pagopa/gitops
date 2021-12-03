bb6bdb7c0a6c875c7fcb5d040c0a385d04fb4f1c
anttorre <antonio.torre@everis.com>
2021-12-03 14:45:27 +0100

    tc file renamed into terms-condition + env in english

--- azure-devops/projects/selfcare-projects/selc-fe-login.tf
+++ azure-devops/projects/selfcare-projects/selc-fe-login.tf
@@ -33,12 +33,12 @@ locals {
   }
   # deploy vars
   selc-fe-login-variables_deploy = {
-    dev_react_app_url_file_informativa_privacy  = "https://dev.selfcare.pagopa.it/assets/InformativaPrivacy.pdf"
-    dev_react_app_url_file_termini_e_condizioni = "https://dev.selfcare.pagopa.it/assets/tc.pdf"
-    uat_react_app_url_file_informativa_privacy  = "https://uat.selfcare.pagopa.it/assets/InformativaPrivacy.pdf"
-    uat_react_app_url_file_termini_e_condizioni = "https://uat.selfcare.pagopa.it/assets/tc.pdf"
-    prod_react_app_url_file_informativa_privacy  = "https://selfcare.pagopa.it/assets/InformativaPrivacy.pdf"
-    prod_react_app_url_file_termini_e_condizioni = "https://selfcare.pagopa.it/assets/tc.pdf"
+    dev_react_app_url_file_privacy_disclaimer  = "https://dev.selfcare.pagopa.it/assets/InformativaPrivacy.pdf"
+    dev_react_app_url_file_terms_and_conditions = "https://dev.selfcare.pagopa.it/assets/terms-condition.pdf"
+    uat_react_app_url_file_privacy_disclaimer  = "https://uat.selfcare.pagopa.it/assets/InformativaPrivacy.pdf"
+    uat_react_app_url_file_terms_and_conditions = "https://uat.selfcare.pagopa.it/assets/terms-condition.pdf"
+    prod_react_app_url_file_privacy_disclaimer  = "https://selfcare.pagopa.it/assets/InformativaPrivacy.pdf"
+    prod_react_app_url_file_terms_and_conditions = "https://selfcare.pagopa.it/assets/terms-condition.pdf"
   }
   # deploy secrets
   selc-fe-login-variables_secret_deploy = {
