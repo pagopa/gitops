1e971f1a01bf205158b8239c19defd242ce393de
anttorre <antonio.torre@everis.com>
2022-01-25 16:58:39 +0100

    SELC-409 - assistance fe devops

--- azure-devops/projects/selfcare-projects/locals.tf
+++ azure-devops/projects/selfcare-projects/locals.tf
@@ -30,11 +30,14 @@ locals {
     prod_resource_group     = "selc-p-checkout-fe-rg"
     prod_storage_account    = "selcpcheckoutsa"
 
+    react_app_assistance_email = "selfcare@assistenza.pagopa.it"
+
     dev_react_app_url_cdn                      = "https://dev.selfcare.pagopa.it"
     dev_react_app_url_storage                  = "https://selcdcheckoutsa.z6.web.core.windows.net"
     dev_react_app_url_fe_login                 = "https://dev.selfcare.pagopa.it/auth"
     dev_react_app_url_fe_onboarding            = "https://dev.selfcare.pagopa.it/onboarding"
     dev_react_app_url_fe_dashboard             = "https://dev.selfcare.pagopa.it/dashboard"
+    dev_react_app_url_fe_assistenza            = "https://dev.selfcare.pagopa.it/assistenza"
     dev_react_app_url_fe_landing               = "https://dev.selfcare.pagopa.it/auth/logout" // TODO when the landing will exists, replace this with the correct URL
     dev_react_app_url_api_login                = "https://api.dev.selfcare.pagopa.it/spid/v1"
     dev_react_app_url_api_party_process        = "https://api.dev.selfcare.pagopa.it/party-process/v1"
@@ -42,12 +45,14 @@ locals {
     dev_react_app_url_api_party_registry_proxy = "https://api.dev.selfcare.pagopa.it/party-registry-proxy/v1"
     dev_react_app_url_api_dashboard            = "https://api.dev.selfcare.pagopa.it/dashboard/v1"
     dev_react_app_url_api_onboarding           = "https://api.dev.selfcare.pagopa.it/onboarding/v1"
+    dev_react_app_url_api_notification         = "https://api.dev.selfcare.pagopa.it/notifications/v1"
 
     uat_react_app_url_cdn                      = "https://uat.selfcare.pagopa.it"
     uat_react_app_url_storage                  = "https://selcucheckoutsa.z6.web.core.windows.net"
     uat_react_app_url_fe_login                 = "https://uat.selfcare.pagopa.it/auth"
     uat_react_app_url_fe_onboarding            = "https://uat.selfcare.pagopa.it/onboarding"
     uat_react_app_url_fe_dashboard             = "https://uat.selfcare.pagopa.it/dashboard"
+    uat_react_app_url_fe_assistance            = "https://uat.selfcare.pagopa.it/assistenza"
     uat_react_app_url_fe_landing               = "https://uat.selfcare.pagopa.it/auth/logout" // TODO when the landing will exists, replace this with the correct URL
     uat_react_app_url_api_login                = "https://api.uat.selfcare.pagopa.it/spid/v1"
     uat_react_app_url_api_party_process        = "https://api.uat.selfcare.pagopa.it/party-process/v1"
@@ -55,12 +60,14 @@ locals {
     uat_react_app_url_api_party_registry_proxy = "https://api.uat.selfcare.pagopa.it/party-registry-proxy/v1"
     uat_react_app_url_api_dashboard            = "https://api.uat.selfcare.pagopa.it/dashboard/v1"
     uat_react_app_url_api_onboarding           = "https://api.uat.selfcare.pagopa.it/onboarding/v1"
+    uat_react_app_url_api_notification         = "https://api.uat.selfcare.pagopa.it/notifications/v1"
 
     prod_react_app_url_cdn                      = "https://prod.selfcare.pagopa.it"
     prod_react_app_url_storage                  = "https://selcpcheckoutsa.z6.web.core.windows.net"
     prod_react_app_url_fe_login                 = "https://selfcare.pagopa.it/auth"
     prod_react_app_url_fe_onboarding            = "https://selfcare.pagopa.it/onboarding"
     prod_react_app_url_fe_dashboard             = "https://selfcare.pagopa.it/dashboard"
+    prod_react_app_url_fe_assistance            = "https://selfcare.pagopa.it/assistenza"
     prod_react_app_url_fe_landing               = "https://selfcare.pagopa.it/auth/logout" // TODO when the landing will exists, replace this with the correct URL
     prod_react_app_url_api_login                = "https://api.selfcare.pagopa.it/spid/v1"
     prod_react_app_url_api_party_process        = "https://api.selfcare.pagopa.it/party-process/v1"
@@ -68,5 +75,6 @@ locals {
     prod_react_app_url_api_party_registry_proxy = "https://api.selfcare.pagopa.it/party-registry-proxy/v1"
     prod_react_app_url_api_dashboard            = "https://api.selfcare.pagopa.it/dashboard/v1"
     prod_react_app_url_api_onboarding           = "https://api.selfcare.pagopa.it/onboarding/v1"
+    prod_react_app_url_api_notification         = "https://api.selfcare.pagopa.it/notifications/v1"
   }
 }
