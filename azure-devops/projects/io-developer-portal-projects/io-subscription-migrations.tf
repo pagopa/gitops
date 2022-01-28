6cd8ef302bf11f801932598ff0d2d00e51307a5f
Emanuele De Cupis <emanuele.decupis@pagopa.it>
2022-01-28 11:35:31 +0100

    [#IC-188] Add code-review pipeline definition for `io-subscription-migrations` (#225)
    * add code-review pipeline definition
    
    * Update azure-devops/projects/io-developer-portal-projects/io-subscription-migrations.tf
    
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
    
    * [#IC-188] Add deploy pipeline for `io-subsriprion-migrations` (#232)
    
    * added pipeline to handle selfcare-common-frontend project (#228)
    
    * added pipeline to handle selfcare-common-frontend project
    
    * fixed duplicate var name
    
    * add-secret
    
    Co-authored-by: anttorre <antonio.torre@everis.com>
    Co-authored-by: pasqualedevita <>
    
    * Update CODEOWNERS
    
    * added npm_connection deploy var (#229)
    
    Co-authored-by: anttorre <antonio.torre@everis.com>
    
    * add env with api key to run api tests (#226)
    
    * [PPR-290] Code-review pipeline for nodo-dei-pagamenti-test (#227)
    
    * added vars for code-review pipeline regarding nodo-dei-pagamenti-test project
    
    * fix storage account and resource group names
    
    * added kv vars
    
    * removed sas vars
    
    * add deploy pipeline for io-subsriprion-migrations
    
    * Selfcare assistance services (#231)
    
    * SELC-409 - assistance fe devops
    
    * SELC-410 - notification-manager be devops
    
    * SELC-409 fixed dev_ assistance variable name
    
    Co-authored-by: anttorre <antonio.torre@everis.com>
    
    * fix git repo name
    
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>
    Co-authored-by: anttorre <antonio.torre@everis.com>
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>
    Co-authored-by: Francesco Cesareo <cesareo.francesco@gmail.com>
    Co-authored-by: uolter <walter.traspadini@pagopa.it>
    
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>
    Co-authored-by: anttorre <antonio.torre@everis.com>
    Co-authored-by: Simone infante <52280205+infantesimone@users.noreply.github.com>
    Co-authored-by: Francesco Cesareo <cesareo.francesco@gmail.com>
    Co-authored-by: uolter <walter.traspadini@pagopa.it>

---
+++ azure-devops/projects/io-developer-portal-projects/io-subscription-migrations.tf
@@ -0,0 +1,187 @@
+variable "io-subscription-migrations" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "io-subscription-migration"
+      branch_name    = "main"
+      pipelines_path = ".devops"
+    }
+    pipeline = {
+      cache_version_id               = "v1"
+      production_resource_group_name = "io-p-selfcare-be-rg"
+      production_app_name            = "io-p-subsmigrations-fn"
+    }
+  }
+}
+
+# code review
+resource "azuredevops_build_definition" "io-subscription-migrations-code-review" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-pr, azuredevops_project.project]
+
+  project_id = azuredevops_project.project.id
+  name       = "${var.io-subscription-migrations.repository.name}.code-review"
+  path       = "\\${var.io-subscription-migrations.repository.name}"
+
+  pull_request_trigger {
+    initial_branch = var.io-subscription-migrations.repository.branch_name
+    forks {
+      enabled       = false
+      share_secrets = false
+    }
+    override {
+      auto_cancel = false
+      branch_filter {
+        include = [var.io-subscription-migrations.repository.branch_name]
+      }
+      path_filter {
+        exclude = []
+        include = []
+      }
+    }
+  }
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.io-subscription-migrations.repository.organization}/${var.io-subscription-migrations.repository.name}"
+    branch_name           = var.io-subscription-migrations.repository.branch_name
+    yml_path              = "${var.io-subscription-migrations.repository.pipelines_path}/code-review-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  }
+
+  variable {
+    name           = "DEFAULT_BRANCH"
+    value          = var.io-subscription-migrations.repository.branch_name
+    allow_override = false
+  }
+
+  variable {
+    name           = "CACHE_VERSION_ID"
+    value          = var.io-subscription-migrations.pipeline.cache_version_id
+    allow_override = false
+  }
+
+  variable {
+    name           = "DANGER_GITHUB_API_TOKEN"
+    secret_value   = module.secrets.values["DANGER-GITHUB-API-TOKEN"].value
+    is_secret      = true
+    allow_override = false
+  }
+}
+
+# code review serviceendpoint authorization
+resource "azuredevops_resource_authorization" "io-subscription-migrations-code-review-github-ro-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-ro, azuredevops_build_definition.io-subscription-migrations-code-review, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id
+  definition_id = azuredevops_build_definition.io-subscription-migrations-code-review.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+resource "azuredevops_resource_authorization" "io-subscription-migrations-code-review-github-pr-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-pr, azuredevops_build_definition.io-subscription-migrations-code-review, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  definition_id = azuredevops_build_definition.io-subscription-migrations-code-review.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+
+# deploy
+resource "azuredevops_build_definition" "io-subscription-migrations-deploy" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_serviceendpoint_azurerm.PROD-IO, azuredevops_project.project]
+
+  project_id = azuredevops_project.project.id
+  name       = "${var.io-subscription-migrations.repository.name}.deploy"
+  path       = "\\${var.io-subscription-migrations.repository.name}"
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.io-subscription-migrations.repository.organization}/${var.io-subscription-migrations.repository.name}"
+    branch_name           = var.io-subscription-migrations.repository.branch_name
+    yml_path              = "${var.io-subscription-migrations.repository.pipelines_path}/deploy-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  }
+
+  variable {
+    name           = "DEFAULT_BRANCH"
+    value          = var.io-subscription-migrations.repository.branch_name
+    allow_override = false
+  }
+
+  variable {
+    name           = "CACHE_VERSION_ID"
+    value          = var.io-subscription-migrations.pipeline.cache_version_id
+    allow_override = false
+  }
+
+  variable {
+    name           = "GIT_EMAIL"
+    value          = module.secrets.values["io-azure-devops-github-EMAIL"].value
+    allow_override = false
+  }
+
+  variable {
+    name           = "GIT_USERNAME"
+    value          = module.secrets.values["io-azure-devops-github-USERNAME"].value
+    allow_override = false
+  }
+
+  variable {
+    name           = "GITHUB_CONNECTION"
+    value          = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.service_endpoint_name
+    allow_override = false
+  }
+
+  variable {
+    name           = "PRODUCTION_AZURE_SUBSCRIPTION"
+    value          = azuredevops_serviceendpoint_azurerm.PROD-IO.service_endpoint_name
+    allow_override = false
+  }
+
+  variable {
+    name           = "PRODUCTION_APP_NAME"
+    value          = var.io-subscription-migrations.pipeline.production_app_name
+    allow_override = false
+  }
+
+  variable {
+    name           = "PRODUCTION_RESOURCE_GROUP_NAME"
+    value          = var.io-subscription-migrations.pipeline.production_resource_group_name
+    allow_override = false
+  }
+}
+
+# deploy serviceendpoint authorization
+resource "azuredevops_resource_authorization" "io-subscription-migrations-deploy-github-ro-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-ro, azuredevops_build_definition.io-subscription-migrations-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id
+  definition_id = azuredevops_build_definition.io-subscription-migrations-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+resource "azuredevops_resource_authorization" "io-subscription-migrations-deploy-github-rw-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_build_definition.io-subscription-migrations-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  definition_id = azuredevops_build_definition.io-subscription-migrations-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+resource "azuredevops_resource_authorization" "io-subscription-migrations-deploy-azurerm-PROD-IO-auth" {
+  depends_on = [azuredevops_serviceendpoint_azurerm.PROD-IO, azuredevops_build_definition.io-subscription-migrations-deploy, time_sleep.wait]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_azurerm.PROD-IO.id
+  definition_id = azuredevops_build_definition.io-subscription-migrations-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
--- azure-devops/projects/selfcare-projects/locals.tf
+++ azure-devops/projects/selfcare-projects/locals.tf
@@ -46,7 +46,6 @@ locals {
     dev_react_app_url_api_dashboard            = "https://api.dev.selfcare.pagopa.it/dashboard/v1"
     dev_react_app_url_api_onboarding           = "https://api.dev.selfcare.pagopa.it/onboarding/v1"
     dev_react_app_url_api_notification         = "https://api.dev.selfcare.pagopa.it/ms-notification-manager"
-
     uat_react_app_url_cdn                      = "https://uat.selfcare.pagopa.it"
     uat_react_app_url_storage                  = "https://selcucheckoutsa.z6.web.core.windows.net"
     uat_react_app_url_fe_login                 = "https://uat.selfcare.pagopa.it/auth"
@@ -60,7 +59,7 @@ locals {
     uat_react_app_url_api_party_registry_proxy = "https://api.uat.selfcare.pagopa.it/party-registry-proxy/v1"
     uat_react_app_url_api_dashboard            = "https://api.uat.selfcare.pagopa.it/dashboard/v1"
     uat_react_app_url_api_onboarding           = "https://api.uat.selfcare.pagopa.it/onboarding/v1"
-    uat_react_app_url_api_notification         = "https://api.uat.selfcare.pagopa.it/ms-notification-manager"
+    uat_react_app_url_api_notification         = "https://api.uat.selfcare.pagopa.it/notifications/v1"
 
     prod_react_app_url_cdn                      = "https://prod.selfcare.pagopa.it"
     prod_react_app_url_storage                  = "https://selcpcheckoutsa.z6.web.core.windows.net"
@@ -75,6 +74,7 @@ locals {
     prod_react_app_url_api_party_registry_proxy = "https://api.selfcare.pagopa.it/party-registry-proxy/v1"
     prod_react_app_url_api_dashboard            = "https://api.selfcare.pagopa.it/dashboard/v1"
     prod_react_app_url_api_onboarding           = "https://api.selfcare.pagopa.it/onboarding/v1"
-    prod_react_app_url_api_notification         = "https://api.selfcare.pagopa.it/ms-notification-manager"
+    prod_react_app_url_api_notification         = "https://api.selfcare.pagopa.it/notifications/v1"
+
   }
 }
