04b290e05b56d49569740bcb2bd5bd57757d030a
Simone infante <52280205+infantesimone@users.noreply.github.com>
2022-02-14 19:33:40 +0100

    [IOPAY-394] update main branch (#242)

--- azure-devops/projects/pagopa-projects/pagopa-proxy.tf
+++ azure-devops/projects/pagopa-projects/pagopa-proxy.tf
@@ -3,7 +3,7 @@ variable "pagopa-proxy" {
     repository = {
       organization    = "pagopa"
       name            = "io-pagopa-proxy"
-      branch_name     = "master"
+      branch_name     = "main"
       pipelines_path  = ".devops"
       yml_prefix_name = "pagopa"
     }
