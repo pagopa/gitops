8a590b73ef42abd4950cbde990d02cd35e2ef88d
Simone Infante <simone.infante@pagopa.it>
2022-02-21 16:27:04 +0100

    [IOPAY-485] Add checkout api key for testing

--- azure-devops/projects/pagopa-projects/pagopa-proxy.tf
+++ azure-devops/projects/pagopa-projects/pagopa-proxy.tf
@@ -34,12 +34,13 @@ locals {
   }
   # deploy vars
   pagopa-proxy-variables_deploy = {
-    git_mail                = module.secrets.values["io-azure-devops-github-EMAIL"].value
-    git_username            = module.secrets.values["io-azure-devops-github-USERNAME"].value
-    github_connection       = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.service_endpoint_name
-    dev_azure_subscription  = azuredevops_serviceendpoint_azurerm.DEV-PAGOPA.service_endpoint_name
-    uat_azure_subscription  = azuredevops_serviceendpoint_azurerm.UAT-PAGOPA.service_endpoint_name
-    prod_azure_subscription = azuredevops_serviceendpoint_azurerm.PROD-PAGOPA.service_endpoint_name
+    git_mail                 = module.secrets.values["io-azure-devops-github-EMAIL"].value
+    git_username             = module.secrets.values["io-azure-devops-github-USERNAME"].value
+    github_connection        = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.service_endpoint_name
+    dev_azure_subscription   = azuredevops_serviceendpoint_azurerm.DEV-PAGOPA.service_endpoint_name
+    uat_azure_subscription   = azuredevops_serviceendpoint_azurerm.UAT-PAGOPA.service_endpoint_name
+    prod_azure_subscription  = azuredevops_serviceendpoint_azurerm.PROD-PAGOPA.service_endpoint_name
+    checkout_testing_api_key = module.secrets.values["CHECKOUT-TESTING-API-KEY"].value
   }
   # deploy secrets
   pagopa-proxy-variables_secret_deploy = {
