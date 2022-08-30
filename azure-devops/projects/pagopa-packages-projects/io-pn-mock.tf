2daf57a8d50d9b48a7e2fbb6394b0864ea4e7d18
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-08-30 19:38:44 +0200

    fix io-pn-mock

--- azure-devops/projects/pagopa-packages-projects/io-pn-mock.tf
+++ azure-devops/projects/pagopa-packages-projects/io-pn-mock.tf
@@ -9,6 +9,7 @@ variable "io-pn-mock" {
     }
     pipeline = {
       enable_code_review = true
+      enable_deploy      = false
     }
   }
 }
@@ -30,16 +31,29 @@ locals {
   io-pn-mock-variables_secret_code_review = {
     danger_github_api_token = module.secrets.values["DANGER-GITHUB-API-TOKEN"].value
   }
+  # deploy vars
+  io-pn-mock-variables_deploy = {
+
+  }
+  # deploy secret vars
+  io-pn-mock-variables_secret_deploy = {
+
+  }
 }
 
+#
+# Code Review
+#
 module "io-pn-mock_code_review" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.6.2"
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=fix-pipelines-names"
   count  = var.io-pn-mock.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = azuredevops_project.project.id
   repository                   = var.io-pn-mock.repository
   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
   path                         = var.io-pn-mock.repository.name
+
+  pull_request_trigger_use_yaml = true
 
   variables = merge(
     local.io-pn-mock-variables,
@@ -49,6 +63,33 @@ module "io-pn-mock_code_review" {
   variables_secret = merge(
     local.io-pn-mock-variables_secret,
     local.io-pn-mock-variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.io-azure-devops-github-ro.id,
+  ]
+}
+
+#
+# Deploy
+#
+module "io-pn-mock-variables_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=fix-pipelines-names"
+  count  = var.io-pn-mock.pipeline.enable_deploy == true ? 1 : 0
+
+  project_id                   = azuredevops_project.project.id
+  repository                   = var.io-pn-mock.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  path                         = var.io-pn-mock.repository.name
+
+  variables = merge(
+    local.io-pn-mock-variables,
+    local.io-pn-mock-variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.io-pn-mock-variables_secret,
+    local.io-pn-mock-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
