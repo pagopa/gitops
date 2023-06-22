35464a74c20bc3f25dc22fab9827ee048fcbf71c
garma00 <giovanniarmaleo00@gmail.com>
2023-06-22 10:03:08 +0200

    Add deploy pipeline

--- azure-devops/projects/io-backend-projects/io-openid-provider.tf
+++ azure-devops/projects/io-backend-projects/io-openid-provider.tf
@@ -78,3 +78,103 @@ resource "azuredevops_resource_authorization" "io-openid-provider-code-review-gi
   authorized    = true
   type          = "endpoint"
 }
+
+#
+# Deploy pipeline
+#
+
+# Define deploy pipeline
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_serviceendpoint_azurerm.PROD-IO, azuredevops_project.project]
+resource "azuredevops_build_definition" "io-openid-provider-deploy" {
+
+  project_id = azuredevops_project.project.id
+  name       = "${var.io-openid-provider.repository.name}.deploy"
+  path       = "\\${var.io-openid-provider.repository.name}"
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "${var.io-openid-provider.repository.organization}/${var.io-openid-provider.repository.name}"
+    branch_name           = var.io-openid-provider.repository.branch_name
+    yml_path              = "${var.io-openid-provider.repository.pipelines_path}/deploy-pipelines.yml"
+    service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  }
+
+  variable {
+    name  = "GIT_EMAIL"
+    value = module.secrets.values["io-azure-devops-github-EMAIL"].value
+  }
+
+  variable {
+    name  = "GIT_USERNAME"
+    value = module.secrets.values["io-azure-devops-github-USERNAME"].value
+  }
+
+  variable {
+    name  = "GITHUB_CONNECTION"
+    value = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.service_endpoint_name
+  }
+
+  variable {
+    name  = "CACHE_VERSION_ID"
+    value = var.io-openid-provider.pipeline.cache_version_id
+  }
+
+  variable {
+    name  = "PRODUCTION_AZURE_SUBSCRIPTION"
+    value = azuredevops_serviceendpoint_azurerm.PROD-IO.service_endpoint_name
+  }
+
+  variable {
+    name  = "NPM_CONNECTION"
+    value = azuredevops_serviceendpoint_npm.pagopa-npm-bot.service_endpoint_name
+  }
+
+  variable {
+    name  = "AGENT_POOL"
+    value = local.agent_pool
+  }
+}
+
+# Allow deploy pipeline to access Github readonly service connection, needed to access external templates to be used inside the pipeline
+resource "azuredevops_resource_authorization" "io-openid-provider-deploy-github-ro-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.pagopa, azuredevops_build_definition.io-openid-provider-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.pagopa.id
+  definition_id = azuredevops_build_definition.io-openid-provider-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow deploy pipeline to access Github writable service connection, needed to bump project version and publish a new relase
+resource "azuredevops_resource_authorization" "io-openid-provider-deploy-github-rw-auth" {
+  depends_on = [azuredevops_serviceendpoint_github.io-azure-devops-github-rw, azuredevops_build_definition.io-openid-provider-deploy, azuredevops_project.project]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+  definition_id = azuredevops_build_definition.io-openid-provider-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow deploy pipeline to access Azure PROD-IO subscription service connection, needed to interact with Azure resources
+resource "azuredevops_resource_authorization" "io-openid-provider-deploy-azurerm-PROD-IO-auth" {
+  depends_on = [azuredevops_serviceendpoint_azurerm.PROD-IO, azuredevops_build_definition.io-openid-provider-deploy, time_sleep.wait]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_azurerm.PROD-IO.id
+  definition_id = azuredevops_build_definition.io-openid-provider-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
+
+# Allow deploy pipeline to access NPM service connection, needed to publish sdk packages to the public registry
+resource "azuredevops_resource_authorization" "io-openid-provider-deploy-npm-auth" {
+  depends_on = [azuredevops_serviceendpoint_npm.pagopa-npm-bot, azuredevops_build_definition.io-openid-provider-deploy, time_sleep.wait]
+
+  project_id    = azuredevops_project.project.id
+  resource_id   = azuredevops_serviceendpoint_npm.pagopa-npm-bot.id
+  definition_id = azuredevops_build_definition.io-openid-provider-deploy.id
+  authorized    = true
+  type          = "endpoint"
+}
